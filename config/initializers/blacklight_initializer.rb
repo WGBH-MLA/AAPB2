0a80d1ff26abdb91d7a395972c21bc864d7a3d36
Chuck McCallum <chuck_mccallum@wgbh.org>
2014-11-06 14:29:45 -0500

    Out-of-the-box rails and blacklight install. For blacklight, left off "--devise --marc".

---
+++ config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = 'e664550f350a93a9027045fd880a48648345d072a0986389644dc57ba3ab69bdaa355c6c522ac2a9de63ef901e30395c3ae5dda39d7ec14f5c687c285595daff'
+
