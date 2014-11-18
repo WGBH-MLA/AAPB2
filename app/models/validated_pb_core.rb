c39ee995e1a9d607f69b1bf9ce55c258318c71fe
Chuck McCallum <chuck_mccallum@wgbh.org>
2014-11-18 17:26:02 -0500

    split validation into methods.

--- app/models/validated_pb_core.rb
+++ app/models/validated_pb_core.rb
@@ -6,18 +6,27 @@ class ValidatedPBCore < PBCore
   
   def initialize(xml)
     super(xml)
+    schema_validate(xml)
+    method_validate
+  end
+  
+  private
+  
+  def schema_validate(xml)
     document = Nokogiri::XML(xml)
     errors = @@schema.validate(document)
     if !errors.empty?
       raise errors.join("\n")
     end
-    
-    # Warm the object and check for missing data, beyond what the schema enforces:
+  end
+  
+  def method_validate
+    # Warm the object and check for missing data, beyond what the schema enforces.
+    errors = []
     (PBCore.instance_methods(false)-[:to_solr]).each do |method|
       begin
         self.send(method)
       rescue => e
-        #binding.pry
         errors << e.message
       end
     end
@@ -25,4 +34,5 @@ class ValidatedPBCore < PBCore
       raise errors.join("\n")
     end
   end
+  
 end
