e10fb79c636744e42d0e9e8b8139dca4d8f680e6
Chuck McCallum <chuck_mccallum@wgbh.org>
2014-11-19 16:25:52 -0500

    Make errors more helpful.

--- scripts/unzip_clean_validate.rb
+++ scripts/unzip_clean_validate.rb
@@ -2,6 +2,12 @@ require_relative 'unzipper'
 require_relative 'cleaner'
 require_relative '../app/models/validated_pb_core'
 
+class Exception
+  def short
+    self.message + "\n" + self.backtrace[0..2].join("\n")
+  end
+end
+
 if __FILE__ == $0
   if ARGV.count != 1
     abort "Expects one argument, not #{ARGV.count}"
@@ -9,12 +15,18 @@ if __FILE__ == $0
   
   unzipper = Unzipper.new(ARGV[0])
   unzipper.each do |dirty_xml|
-    clean_xml = Cleaner.clean(dirty_xml)
+    begin
+      clean_xml = Cleaner.clean(dirty_xml)
+    rescue => e
+      abort "Cleaner died:\n#{dirty_xml}\n#{e.short}"
+    end
+    
     begin
       pbcore = ValidatedPBCore.new(clean_xml)
-      puts "VALID: #{pbcore.id}"
     rescue => e
-      abort "This isn't clean enough:\nBEFORE:#{dirty_xml}\nAFTER:\n#{clean_xml}\n#{e}"
+      abort "ValidatedPBCore died:\nBEFORE:#{dirty_xml}\nAFTER:\n#{clean_xml}\n#{e.short}"
     end
+    
+    puts "VALID: #{pbcore.id}"
   end
 end
