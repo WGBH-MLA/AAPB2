ae8ac684402a1a859140348bd1f1da5be3113430
Chuck McCallum <chuck_mccallum@wgbh.org>
2014-11-19 17:20:33 -0500

    Failing test for data we see in the wild.

--- spec/scripts/cleaner_spec.rb
+++ spec/scripts/cleaner_spec.rb
@@ -2,11 +2,14 @@ require_relative '../../scripts/cleaner'
 
 describe Cleaner do
   
-  it 'cleans' do
-    actual = File.read('spec/fixtures/pbcore/actual-1.xml')
-    clean = File.read('spec/fixtures/pbcore/clean-1.xml')
-    
-    expect(Cleaner.clean(actual)).to eq(clean)
+  Dir['spec/fixtures/pbcore/actual-*.xml'].each do |path_actual|
+    it "cleans #{File.basename(path_actual)}" do
+      path_clean = path_actual.gsub('actual', 'clean')
+      actual = File.read(path_actual)
+      clean = File.read(path_clean)
+
+      expect(Cleaner.clean(actual)).to eq(clean)
+    end
   end
   
 end
