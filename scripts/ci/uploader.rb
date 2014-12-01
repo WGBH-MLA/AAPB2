e5357f1b6022db026a12f908674c1d7843baff55
Chuck McCallum <chuck_mccallum@wgbh.org>
2014-12-01 14:14:24 -0500

    Waiting for API key.

--- scripts/ci/uploader.rb
+++ scripts/ci/uploader.rb
@@ -1,3 +1,6 @@
+require 'yaml'
+require 'base64'
 
-
-puts File.read(File.dirname(File.dirname(File.dirname(__FILE__))) + '/config/ci.yml')
+creds = YAML.load_file(File.dirname(File.dirname(File.dirname(__FILE__))) + '/config/ci.yml')
+base64 = Base64.encode64("#{creds['username']}:#{creds['password']}")
+# TODO: get developer key
