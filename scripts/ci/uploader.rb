0436002bb1ce89d695d5b5567dc8aa10f79b782d
Chuck McCallum <chuck_mccallum@wgbh.org>
2014-12-01 15:06:17 -0500

    Works!

--- scripts/ci/uploader.rb
+++ scripts/ci/uploader.rb
@@ -2,5 +2,14 @@ require 'yaml'
 require 'base64'
 
 creds = YAML.load_file(File.dirname(File.dirname(File.dirname(__FILE__))) + '/config/ci.yml')
-base64 = Base64.encode64("#{creds['username']}:#{creds['password']}")
-# TODO: get developer key
+base64 = Base64.encode64("#{creds['username']}:#{creds['password']}").strip
+
+url = 'https://api.cimediacloud.com/oauth2/token'
+auth_header = "Authorization: Basic #{base64}"
+content_header = "Content-Type: application/x-www-form-urlencoded"
+params = "grant_type=password&client_id=#{creds['client_id']}&client_secret=#{creds['client_secret']}"
+
+curl = "curl -XPOST -i '#{url}' -H '#{auth_header}' -H '#{content_header}' -d '#{params}'"
+
+puts curl
+puts `#{curl}`
