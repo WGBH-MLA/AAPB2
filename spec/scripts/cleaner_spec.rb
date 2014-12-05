e59a6781353485b12d13b5c50b8163ae28776f74
Chuck McCallum <chuck_mccallum@wgbh.org>
2014-12-05 12:33:51 -0500

    Add a couple more cases. One new thing: there are documents we can not fix. We want to make sure they fail in the same way.

--- spec/scripts/cleaner_spec.rb
+++ spec/scripts/cleaner_spec.rb
@@ -1,16 +1,33 @@
 require_relative '../../scripts/cleaner'
+require_relative '../../app/models/validated_pb_core'
 
 describe Cleaner do
   
   cleaner = Cleaner.new
   
-  Dir['spec/fixtures/pbcore/actual-*.xml'].each do |path_actual|
-    it "cleans #{File.basename(path_actual)}" do
-      path_clean = path_actual.gsub('actual', 'clean')
-      actual = File.read(path_actual)
-      clean = File.read(path_clean)
+  describe 'given broken xml (but fixable)' do
+    Dir['spec/fixtures/pbcore/actual-yes-fix-*.xml'].each do |path_actual|
+      it "cleans #{File.basename(path_actual)}" do
+        path_clean = path_actual.gsub('actual-yes-fix', 'clean')
+        actual = File.read(path_actual)
+        clean = File.read(path_clean)
 
-      expect(cleaner.clean(actual)).to eq(clean)
+        expect(cleaner.clean(actual)).to eq(clean)
+        expect{ValidatedPBCore.new(clean)}.not_to raise_error
+      end
+    end
+  end
+
+  describe 'given broken xml (hopeless)' do
+    Dir['spec/fixtures/pbcore/actual-no-fix-*.xml'].each do |path_actual|
+      it "chokes on #{File.basename(path_actual)}" do
+        actual = File.read(path_actual)
+        expect do 
+          # Error could occur in either phase: we don't care.
+          clean = cleaner.clean(actual)
+          ValidatedPBCore.new(clean)
+        end.to raise_error
+      end
     end
   end
   
