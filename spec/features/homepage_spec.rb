325fe42f3da919bebd057682849379d857f93b3d
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-01-12 10:43:08 -0500

    stub home page. No necessary connection to the BL catalog, so making it separate.

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,14 +1,11 @@
 require 'rails_helper'
 
-describe "visiting homepage" do
-
-  before :each do
-    # TODO: ingest object
-  end
+describe "Homepage" do
 
   it "works" do
     visit '/'
     expect(page.status_code).to eq(200)
+    expect(page).to have_text('TODO: home page')
   end
 
 end
