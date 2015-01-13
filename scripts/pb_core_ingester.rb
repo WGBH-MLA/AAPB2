120de69bfdf3d7128bc7c28e3feb20cdc61f8b07
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-01-13 17:22:42 -0500

    New ingester works.

--- scripts/pb_core_ingester.rb
+++ scripts/pb_core_ingester.rb
@@ -38,6 +38,8 @@ class PBCoreIngester
       raise SolrError.new(e)
     end
     
+    pbcore
+    
   end
   
   class ChainedError < StandardError
@@ -57,4 +59,48 @@ class PBCoreIngester
   
 end
 
-# TODO: command-line
+if __FILE__ == $0
+  
+  class String
+    def colorize(color_code)
+      "\e[#{color_code}m#{self}\e[0m"
+    end
+    def red
+      colorize(31)
+    end
+    def green
+      colorize(32)
+    end
+  end
+
+  failed_to_read = []
+  failed_to_validate = []
+  failed_to_add = []
+  success = []
+  
+  ingester = PBCoreIngester.new
+
+  ARGV.each do |path|
+    begin
+      pbcore = ingester.ingest(path)
+    rescue PBCoreIngester::ReadError => e
+      puts "Failed to read '#{path}': #{e.message}".red
+      failed_to_read << path
+    rescue PBCoreIngester::ValidationError => e
+      puts "Failed to validate '#{path}: #{e.message}".red
+      failed_to_validate << path
+    rescue PBCoreIngester::SolrError => e
+      puts "Failed to add '#{path}': #{e.message}".red
+      failed_to_add << path
+    else
+      puts "Successfully added '#{path}' (id:#{pbcore.id})".green
+      success << path
+    end
+  end
+
+  puts "DONE"
+  puts "#{failed_to_read.count} failed to load" if !failed_to_read.empty?
+  puts "#{failed_to_validate.count} failed to validate" if !failed_to_validate.empty?
+  puts "#{failed_to_add.count} failed to add" if !failed_to_add.empty?
+  puts "#{success.count} succeeded"
+end
