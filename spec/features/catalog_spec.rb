3df18910604b6af590726c7a612b08f97ade5d6e
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-01-29 17:08:51 -0500

    Test facets and field search.

--- spec/features/catalog_spec.rb
+++ spec/features/catalog_spec.rb
@@ -29,6 +29,49 @@ describe 'Catalog' do
       end
     end
     
+    describe 'facets' do
+      [
+        ['media_type','Sound',6],
+        ['genre','Interview',3],
+        ['asset_type','Segment',5],
+        ['organization_code','WGBH',1],
+        ['year','2000',1]
+      ].each do |(facet,value,count)|
+        url = "/catalog?f[#{facet}][]=#{value}"
+        it "#{facet}=#{value}: #{count}\t#{url}" do
+          visit url
+          expect(page.status_code).to eq(200)
+          expect_count(count)
+        end
+      end
+    end
+    
+    describe 'fields' do
+      [
+        ['all_fields','Larry',2],
+        ['title','Larry',1],
+        ['contrib','Larry',1]
+      ].each do |(constraint,value,count)|
+        url = "/catalog?search_field=#{constraint}&q=#{value}"
+        it "#{constraint}=#{value}: #{count}\t#{url}" do
+          visit url
+          expect(page.status_code).to eq(200)
+          expect_count(count)
+        end
+      end
+    end
+    
+    def expect_count(count)
+      case count
+      when 0
+        expect(page).to have_text("No entries found")
+      when 1
+        expect(page).to have_text("1 entry found")
+      else 
+        expect(page).to have_text("1 - #{count} of #{count}")
+      end
+    end
+    
   end
   
   describe '#show' do
