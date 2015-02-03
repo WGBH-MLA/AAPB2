0265d5df1308572b253d7b4f739f140faf9a1dc0
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-02-03 14:35:46 -0500

    asset_dates

--- spec/models/pb_core_spec.rb
+++ spec/models/pb_core_spec.rb
@@ -83,6 +83,7 @@ describe 'Validated and plain PBCore' do
         },
         asset_type: 'Album',
         asset_date: '2000-01-01',
+        asset_dates: {nil=>'2000-01-01'}, # TODO: should this have mapped to "uncataloged"?
         titles: {"Program"=>"Gratuitous Explosions", "Series"=>"NOVA"},
         title: 'Gratuitous Explosions',
         descriptions: ['Best episode ever!'],
