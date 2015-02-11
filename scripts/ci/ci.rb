9df33ce5178a1350a37dae4e2b33163394868693
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-02-11 16:30:07 -0500

    Factor out the core functionality for Ci.

--- scripts/ci/ci.rb
+++ scripts/ci/ci.rb
@@ -1,58 +1,12 @@
 require 'yaml'
 require 'curb'
 require 'json'
+require_relative '../../app/models/ci_core'
 
-class Ci
-  
-  include Enumerable
-
-  attr_reader :access_token
-  attr_reader :verbose
-  attr_reader :workspace_id
-  
-  def initialize(opts={})
-    unrecognized_opts = opts.keys - [:verbose, :credentials_path, :credentials]
-    raise "Unrecognized options #{unrecognized_opts}" unless unrecognized_opts == []
-    
-    @verbose = opts[:verbose] ? true : false
-    
-    raise 'Credentials specified twice' if opts[:credentials_path] && opts[:credentials]
-    raise 'No credentials given' if !opts[:credentials_path] && !opts[:credentials]
-    credentials = opts[:credentials] || YAML.load_file(opts[:credentials_path])
-    
-    credentials.keys.sort.tap { |actual|
-      expected = ['username', 'password', 'client_id', 'client_secret', 'workspace_id'].sort
-      raise "Expected #{expected} in ci credentials, not #{actual}" if actual != expected
-    }
-    
-    params = {
-      'grant_type' => 'password',
-      'client_id' => credentials['client_id'],
-      'client_secret' => credentials['client_secret']
-    }.map { |k,v| Curl::PostField.content(k,v) }
-
-    curl = Curl::Easy.http_post('https://api.cimediacloud.com/oauth2/token', *params) do |c|
-      c.verbose = @verbose
-      c.http_auth_types = :basic
-      c.username = credentials['username']
-      c.password = credentials['password']
-      # c.on_missing { |curl, data| puts "4xx: #{data}" }
-      # c.on_failure { |curl, data| puts "5xx: #{data}" }
-      c.perform
-    end
-
-    @access_token = JSON.parse(curl.body_str)['access_token']
-    raise 'OAuth failed' unless @access_token
-    
-    @workspace_id = credentials['workspace_id']
-  end
+class Ci < CiCore
   
   def upload(file_path, log_file)
     Uploader.new(self, file_path, log_file).upload
-  end
-  
-  def download(asset_id)
-    Downloader.new(self).download(asset_id)
   end
   
   def list(limit=50, offset=0)
@@ -142,28 +96,6 @@ class Ci
         assets.each{|asset| yield asset}
         offset += limit
       end
-    end
-    
-  end
-  
-  class Downloader < CiClient
-    
-    @@cache = {}
-    
-    def initialize(ci)
-      @ci = ci
-    end
-    
-    def download(asset_id)
-      hit = @@cache[asset_id]
-      if !hit || hit[:expires] < Time.now
-        curl = Curl::Easy.http_get("https""://api.cimediacloud.com/assets/#{asset_id}/download") do |c|
-          perform(c)
-        end
-        url = JSON.parse(curl.body_str)['location']
-        @@cache[asset_id] = {url: url, expires: Time.now + 3*60*60}
-      end
-      @@cache[asset_id][:url]
     end
     
   end
