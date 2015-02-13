c773eb89bf0483c00d58be60afca9e9874cdd186
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-02-13 12:15:44 -0500

    Shorter label

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -70,7 +70,7 @@ class CatalogController < ApplicationController
     
     VocabMap.new(File.dirname(File.dirname(File.dirname(__FILE__)))+'/config/vocab-maps/title-type-map.yml').tap{|map|
       map.authorized_names.each{|name|
-        config.add_facet_field "#{name.downcase.gsub(/\s/,'_')}_titles", show: false
+        config.add_facet_field "#{name.downcase.gsub(/\s/,'_')}_titles", show: false, label: name
       }
     }
     
