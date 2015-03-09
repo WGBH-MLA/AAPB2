e619e677906a6475b84b2508cff58bcc49d33606
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-03-09 13:25:59 -0400

    Cache blog... vainly try to get it to shut up during tests.

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -3,7 +3,10 @@ require_relative '../support/validation_helper'
 
 describe 'Homepage' do
   it 'works' do
+    # WP-client gem must have hard coded reference to STDOUT or STDERR:
+    # swapping $stdout and $stderr didn't quiet it.
     visit '/'
+    
     expect(page.status_code).to eq(200)
     expect(page).to have_text('Discover millions of historic programs')
     expect_fuzzy_xml
