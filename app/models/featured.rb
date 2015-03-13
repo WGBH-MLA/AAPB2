308490487869f10e05a9e98b63ca1e0ce695316a
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-03-13 14:39:57 -0400

    Multiple galleries

--- app/models/featured.rb
+++ app/models/featured.rb
@@ -4,23 +4,30 @@ class Featured
   attr_reader :name
   attr_reader :thumbnail_url
 
-  private
-
+  private 
+  
   def initialize(hash)
     @id = hash['id']
     @org_name = hash['org_name']
     @name = hash['name']
-    @thumbnail_url = hash['thumbnail_url'] || "//mlamedia01.wgbh.org/aapb/featured/#{@id}_gallery.jpg"
+    @thumbnail_url = hash['thumbnail_url'] || "http://mlamedia01.wgbh.org/aapb/featured/#{@id}_gallery.jpg"
   end
 
-  (File.dirname(File.dirname(File.dirname(__FILE__))) + '/config/featured.yml').tap do |path|
-    @@features = YAML.load_file(path).map { |hash| Featured.new(hash) }
+  (File.dirname(File.dirname(File.dirname(__FILE__))) + '/config/featured').tap do |parent_path|
+    @@galleries = Hash[
+      Dir["#{parent_path}/*-featured.yml"].map do |gallery_path|
+        [
+          gallery_path.sub(%r{.*/}, '').sub('-featured.yml', ''),
+          YAML.load_file(gallery_path).map { |hash| Featured.new(hash) }
+        ]
+      end
+    ]
   end
   
   public
   
-  def self.all
-    @@features
+  def self.from_gallery(gallery_name)
+    @@galleries[gallery_name]
   end
 
 end
