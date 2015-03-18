5e0522fbd59fc93f5ae59a3c5a6bdc3aa128fa7b
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-03-18 12:02:07 -0400

    Markdown overrides.

--- app/controllers/override_controller.rb
+++ app/controllers/override_controller.rb
@@ -1,9 +1,19 @@
+require_relative '../../lib/carpet_factory'
+
 class OverrideController < ApplicationController
   def show
     if params[:path] =~ /^[a-z0-9\/-]+$/i # paranoid about weird paths.
-      override_file_path = "override/#{params[:path]}.html.erb"
-      if File.exist?("app/views/#{override_file_path}")
-        render file: override_file_path
+      override_html_erb_file_path = "override/#{params[:path]}.html.erb"
+      if File.exist?("app/views/#{override_html_erb_file_path}")
+        render file: override_html_erb_file_path
+        return
+      end
+      
+      override_md_file_path = "override/#{params[:path]}.md"
+      full_path = (File.dirname(File.dirname(__FILE__))) + "/views/#{override_md_file_path}"
+      if File.exist?(full_path)
+        @content = CarpetFactory.render(File.read(full_path))
+        render file: 'override-containers/md-container.erb'
         return
       end
     end
---
+++ app/views/override/test.md
@@ -0,0 +1,8 @@
+# testing 123
+
+- list 1
+- list 2
+- list 3
+
+>
+block-quote
