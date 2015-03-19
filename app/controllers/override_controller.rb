ebb4bfbbb35c06d06cab6a42b0865b580066ffff
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-03-19 10:36:28 -0400

    pass filename to enable caching. tighter regex.

--- app/controllers/override_controller.rb
+++ app/controllers/override_controller.rb
@@ -11,8 +11,8 @@ class OverrideController < ApplicationController
       override_md_file_path = "override/#{params[:path]}.md"
       full_path = (File.dirname(File.dirname(__FILE__))) + "/views/#{override_md_file_path}"
       if File.exist?(full_path)
-        html = Markdown.render(File.read(full_path))
-        (@title, @body) = html.match(%r{(<h1>.*?<\/h1>)(.*)}m).captures
+        html = Markdown.render_file(full_path)
+        (@title, @body) = html.match(%r{^\s*(<h1>.*?<\/h1>)(.*)}m).captures
         # This is wrong, but not worth a full xml parse.
         render file: 'override-containers/md-container.erb'
         return
--- lib/markdown.rb
+++ lib/markdown.rb
@@ -6,4 +6,8 @@ module Markdown
     return unless md_text
     @@markdown.render(md_text)    
   end
+  def self.render_file(md_file_name)
+    # TODO: cache in production environments?
+    self.render(File.read(md_file_name))
+  end
 end
