afcff113875c8c4716be7a28c7e691f49fe543ba
Andrew Myers <afredmyers@gmail.com>
2015-04-03 11:57:36 -0400

    Quiets down the cap deploy logs. Adds a stub for Blacklight initializer secret key thing.

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :deploy_to, '/srv/www/aapb'
 # set :format, :pretty
 
 # Default value for :log_level is :debug
-# set :log_level, :debug
+set :log_level, :info
 
 # Default value for :pty is false
 # set :pty, true
