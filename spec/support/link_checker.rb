3e7afd3ad8cf83abbddc61a9d4ed454cf1233296
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-04-07 19:56:55 -0400

    Tests should pass.

--- spec/support/link_checker.rb
+++ spec/support/link_checker.rb
@@ -3,16 +3,28 @@ require 'set'
 module LinkChecker
   @@checked = Set[]
   FILENAME = File.join(File.dirname(__FILE__),'.link-check.txt')
+  RE_IGNORES = [
+    /^\/catalog\?/, # to many combos
+    /^https?:/, # TODO: remote sites
+    /#/ # TODO: anchors
+  ]
   def self.needs_recheck?()
-    !File.exists?(FILENAME) ||
-      (Time.now - File.mtime(FILENAME))/(60*60*24*7) > 1
+    return true if !File.exists?(FILENAME)
+    if (Time.now - File.mtime(FILENAME))/(60*60*24*7) > 1
+      File.unlink(FILE)
+      return true
+    end
+    return false
   end
   @@needs_recheck = self.needs_recheck?()
   def self.check(url)
-    if @@needs_recheck && !@@checked.include?(url)
-      puts "TODO: check #{url}"
-      @@checked << url
-      File.open(FILENAME, 'a') { |f| f.write(url) }
-    end
+    return unless @@needs_recheck
+    return if @@checked.include?(url)
+    return if RE_IGNORES.map{|ignore| ignore.match(url)}.any?
+
+    fail("relative links are trouble: #{url}") if /^[^\/]/.match(url)
+    # TODO: try fetching with capybara
+    @@checked << url
+    File.open(FILENAME, 'a') { |f| f.write(url) }
   end
 end
--- spec/support/validation_helper_spec.rb
+++ spec/support/validation_helper_spec.rb
@@ -2,24 +2,30 @@ require_relative 'validation_helper'
 require 'ostruct'
 
 describe ValidationHelper do
+  class Fake < OpenStruct
+    def all(_ignored)
+      []
+    end
+  end
+  
   describe 'obvious errors' do
     it 'catches mismatched tags' do
       def page
-        OpenStruct.new(body: '<html><a>TEXT</b></html>')
+        Fake.new(body: '<html><a>TEXT</b></html>')
       end
       expect { expect_fuzzy_xml }.to raise_error
     end
 
     it 'catches missing open brace' do
       def page
-        OpenStruct.new(body: '<html>a>TEXT</a></html>')
+        Fake.new(body: '<html>a>TEXT</a></html>')
       end
       expect { expect_fuzzy_xml }.to raise_error
     end
 
     it 'catches missing close brace' do
       def page
-        OpenStruct.new(body: '<html><aTEXT</a></html>')
+        Fake.new(body: '<html><aTEXT</a></html>')
       end
       expect { expect_fuzzy_xml }.to raise_error
     end
@@ -28,14 +34,14 @@ describe ValidationHelper do
   describe 'tag self closing' do
     it 'closes meta tags' do
       def page
-        OpenStruct.new(body: '<html><meta attribute="value"></html>')
+        Fake.new(body: '<html><meta attribute="value"></html>')
       end
       expect_fuzzy_xml
     end
 
     it 'does not close arbitrary tags' do
       def page
-        OpenStruct.new(body: '<html><arbitrary attribute="value"></html>')
+        Fake.new(body: '<html><arbitrary attribute="value"></html>')
       end
       expect { expect_fuzzy_xml }.to raise_error
     end
@@ -44,21 +50,21 @@ describe ValidationHelper do
   describe 'adding attribute values' do
     it 'adds values' do
       def page
-        OpenStruct.new(body: '<html><arbitrary attribute/></html>')
+        Fake.new(body: '<html><arbitrary attribute/></html>')
       end
       expect_fuzzy_xml
     end
 
     it 'keep previous / double quotes' do
       def page
-        OpenStruct.new(body: '<html><arbitrary prev ="foo" attribute/></html>')
+        Fake.new(body: '<html><arbitrary prev ="foo" attribute/></html>')
       end
       expect_fuzzy_xml
     end
 
     it 'keeps next / single quotes' do
       def page
-        OpenStruct.new(body: '<html><arbitrary attribute next= \'bar\'/></html>')
+        Fake.new(body: '<html><arbitrary attribute next= \'bar\'/></html>')
       end
       expect_fuzzy_xml
     end
