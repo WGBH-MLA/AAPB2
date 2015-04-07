f577fc8a42676ced4a60331a3610b879efe02c2d
Andrew Myers <afredmyers@gmail.com>
2015-04-07 11:45:27 -0400

    Fixes conflicts.

---
+++ lib/middleware/redirect_middleware.rb
@@ -0,0 +1,19 @@
+# http://blog.dynamic50.com/2011/02/22/redirect-all-requests-for-www-to-root-domain-with-heroku/
+
+class RedirectMiddleware
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    request = Rack::Request.new(env)
+    if request.host.starts_with?('www.')
+      [301, {'Location' => request.url.sub('//www.', '//')}, self]
+    else
+      @app.call(env)
+    end
+  end
+
+  def each(&block)
+  end
+end
