8862ce4c0e7c8544bb53015e255c5f46281e1249
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-04-28 16:03:50 -0400

    Activate video player?

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -2,15 +2,19 @@ require_relative '../../lib/access_control'
 
 class MediaController < ApplicationController
   include Blacklight::Catalog
+  
+  CREDENTIALS_PATH = Rails.root + 'config/ci.yml'
 
   def show
-    _response, document = fetch(params['id'])
-    xml = document.instance_variable_get('@_source')['xml']
-    pbcore = PBCore.new(xml)
-
-    ci = CiCore.new(credentials_path: File.dirname(File.dirname(File.dirname(__FILE__))) + '/config/ci.yml')
-    # OAuth credentials expire: otherwise it would make sense to cache this instance.
-    if AccessControl.authorized_ip?(request.remote_ip)
+    if AccessControl.authorized_ip?(request.remote_ip) && 
+        URI.parse(request.referer).host =~ /^(.+\.)?americanarchive\.org$/      
+      ci = CiCore.new(credentials_path: CREDENTIALS_PATH)
+      # OAuth credentials expire: otherwise it would make sense to cache this instance.
+      
+      _response, document = fetch(params['id'])
+      xml = document.instance_variable_get('@_source')['xml']
+      pbcore = PBCore.new(xml)
+      
       redirect_to ci.download(pbcore.ci_id)
     else
       render nothing: true, status: :unauthorized
