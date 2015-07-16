0b1bf234e325f082f9019a8bdc3545a338382a9c
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-07-16 14:56:55 -0400

    Alternate pull left and right to minimize the problem of tall images stacking.

--- app/models/exhibit.rb
+++ app/models/exhibit.rb
@@ -90,7 +90,12 @@ class Exhibit
       
       @summary_html = Exhibit::extract_html(doc, 'Summary')
       @author_html = Exhibit::extract_html(doc, 'Author')
-      @body_html = Exhibit::extract_html(doc, 'Description').gsub('<img ', '<img class="pull-left" ')
+      left = false
+      @body_html = Exhibit::extract_html(doc, 'Description')
+        .gsub('<img ') do |match|
+          left = !left
+          "<img class='pull-#{left ? 'left' : 'right'}' "
+        end
       
       Exhibit::extract_html(doc, 'Links').tap do |links_html|
         Nokogiri::HTML(links_html).tap do |doc|
