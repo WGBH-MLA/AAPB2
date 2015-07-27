f97475edcde8c7e818bed29b0c672a6bca3d354a
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-07-27 18:51:42 -0400

    Seperate the tests.

--- spec/features/override_spec.rb
+++ spec/features/override_spec.rb
@@ -2,19 +2,17 @@ require 'rails_helper'
 require_relative '../support/validation_helper'
 
 describe 'Overrides' do
-  ['override', 'exhibits'].each do |dir|
-    Dir["app/views/#{dir}/**/*"].
-      reject { |file| File.directory?(file) }.
-      reject { |file| file.match(/\.erb$/)}.
-      each do |override|
-        path = override.gsub(/app\/views(\/override)?/, '').sub('.md', '')
+  Dir["app/views/override/**/*"].
+    reject { |file| File.directory?(file) }.
+    reject { |file| file.match(/\.erb$/)}.
+    each do |override|
+      path = override.gsub(/app\/views(\/override)?/, '').sub('.md', '')
 
-        it "#{path} method works" do
-          visit path
-          expect(page.status_code).to eq(200)
-          expect(page.all('input[name="path"]').count).to eq(0)
-          expect_fuzzy_xml
-        end
+      it "#{path} method works" do
+        visit path
+        expect(page.status_code).to eq(200)
+        expect(page.all('input[name="path"]').count).to eq(0)
+        expect_fuzzy_xml
       end
-  end
+    end
 end
