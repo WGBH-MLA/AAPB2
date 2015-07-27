6b8e63f73c8c831fa59370f90ac05bbfc5c67ecb
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-07-27 19:05:40 -0400

    exhibits feature tests

--- app/controllers/exhibits_controller.rb
+++ app/controllers/exhibits_controller.rb
@@ -1,14 +1,14 @@
 require_relative '../models/exhibit'
 
 class ExhibitsController < OverrideController
-  
   def index
     @exhibits = Exhibit.all
     @page_title = 'Exhibits'
   end
-
-  def set_view_fields(path)
-    @exhibit = Exhibit.find_by_path(Exhibit.path_from_file_path(path))
-    @page_title = @exhibit.name
+  
+  def show
+    @exhibit = Exhibit.find_by_path(params[:path])
+    @page_title = @exhibit.title
+    params[:path] = nil 
   end
 end
