56f7125f7c842925b165bdd3ac7d51e043919fab
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-07-27 17:24:59 -0400

    Titles were missing.

--- app/controllers/override_controller.rb
+++ app/controllers/override_controller.rb
@@ -3,6 +3,7 @@ require_relative '../../lib/markdowner'
 class OverrideController < ApplicationController
   def show
     @override = Override.find_by_path(params[:path])
+    @page_title = @override.title
   rescue IndexError 
     fail ActionController::RoutingError.new('404')
   end
