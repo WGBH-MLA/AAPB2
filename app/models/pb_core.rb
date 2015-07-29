b8734527ed476fe8b9135486dc65a6eae398fde8
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-07-29 16:56:07 -0400

    Tests pass locally.

--- app/models/pb_core.rb
+++ app/models/pb_core.rb
@@ -358,8 +358,9 @@ class PBCore # rubocop:disable Metrics/ClassLength
   # These methods are only used by to_solr.
 
   def text
-    ignores = [:text, :to_solr, :contribs, :img_src, :media_srcs, :captions_src, :rights_code, :access_level,
-               :access_types, :titles_sort, :ci_ids, :instantiations, :organization_state_abbreviation]
+    ignores = [:text, :to_solr, :contribs, :img_src, :media_srcs, :captions_src, 
+               :rights_code, :access_level, :access_types, :titles_sort, :ci_ids, 
+               :instantiations, :organization_state_abbreviation, :outside_url]
     @text ||= (PBCore.instance_methods(false) - ignores)
               .reject { |method| method =~ /\?$/ } # skip booleans
               .map { |method| send(method) } # method -> value
