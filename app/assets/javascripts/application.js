b4004fd7553b5b90f3615620dca894d05a95db17
Mike Hamilton <mike@hamiltopia.com>
2015-07-31 15:12:49 -0400

    floating navigation on homepage
    added a floating navigation to relative links on the homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,69 @@
 // Required by Blacklight
 //= require blacklight/blacklight
 //= require_tree .
+
+
+
+
+$(document).ready(function() {
+
+	//Height
+  var howhigh = $(".browsable").height();
+  $('.browsable').css({ 'height': howhigh + "px" });
+  $('.browsable').css({ 'padding': '2em' });
+
+
+	//Browse the catalog filtering serach
+  var jobCount = $('.browsable .in').length;
+  $('.list-count').text(jobCount + ' items');
+    
+  
+  $("#search-text").keyup(function () {
+     //$(this).addClass('hidden');
+  
+    var searchTerm = $("#search-text").val();
+    var listItem = $('.browsable').children('a');
+  
+    
+    var searchSplit = searchTerm.replace(/ /g, "'):containsi('")
+    
+      //extends :contains to be case insensitive
+  $.extend($.expr[':'], {
+  'containsi': function(elem, i, match, array)
+  {
+    return (elem.textContent || elem.innerText || '').toLowerCase()
+    .indexOf((match[3] || "").toLowerCase()) >= 0;
+  }
+});
+    
+    
+    $(".browsable a").not(":containsi('" + searchSplit + "')").each(function(e)   {
+      $(this).addClass('hiding out').removeClass('in');
+      setTimeout(function() {
+          $('.out').addClass('hidden');
+        }, 300);
+    });
+    
+    $(".browsable a:containsi('" + searchSplit + "')").each(function(e) {
+      $(this).removeClass('hidden out').addClass('in');
+      setTimeout(function() {
+          $('.in').removeClass('hiding');
+        }, 1);
+    });
+    
+  
+    var jobCount = $('.browsable .in').length;
+    $('.list-count').text(jobCount + ' items');
+
+    //shows empty state text when no jobs found
+    if(jobCount == '0') {
+      $('.browsable').addClass('empty');
+    }
+    else {
+      $('.browsable').removeClass('empty');
+    }
+    
+  });
+  
+                    
+});
