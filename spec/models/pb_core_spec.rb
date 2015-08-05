33c9d41d6f33d0c89e7883a1c7ccf1131d4dc331
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-08-05 10:30:23 -0400

    Test misused "Level of User Access"

--- spec/models/pb_core_spec.rb
+++ spec/models/pb_core_spec.rb
@@ -39,6 +39,30 @@ describe 'Validated and plain PBCore' do
         expect { ValidatedPBCore.new(invalid_pbcore) }.to(
           raise_error(/Unexpected media types: \["unexpected"\]/))
       end
+      
+      it 'rejects multi "Level of User Access"' do
+        invalid_pbcore = pbc_xml.sub(
+          /<pbcoreAnnotation/,
+          "<pbcoreAnnotation annotationType='Level of User Access'>On Location</pbcoreAnnotation><pbcoreAnnotation")
+        expect { ValidatedPBCore.new(invalid_pbcore) }.to(
+          raise_error(/Should have at most 1 "Level of User Access" annotation/))
+      end
+      
+      it 'rejects digitized w/o "Level of User Access"' do
+        invalid_pbcore = pbc_xml.gsub(
+          /<pbcoreAnnotation annotationType='Level of User Access'>[^<]+<[^>]+>/,
+          '')
+        expect { ValidatedPBCore.new(invalid_pbcore) }.to(
+          raise_error(/Should have "Level of User Access" annotation if digitized/))
+      end
+      
+      it 'rejects undigitized w/ "Level of User Access"' do
+        invalid_pbcore = pbc_xml.gsub(
+          /<pbcoreIdentifier source='Sony Ci'>[^<]+<[^>]+>/,
+          '')
+        expect { ValidatedPBCore.new(invalid_pbcore) }.to(
+          raise_error(/Should not have "Level of User Access" annotation if not digitized/))
+      end
     end
   end
 
