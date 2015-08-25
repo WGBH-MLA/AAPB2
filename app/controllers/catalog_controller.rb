88b9f2776802fd565532897a732836b152ebfda0
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-08-25 13:26:15 -0400

    Exclude state when counting org facets. (Otherwise stations disappear when you choose a state.)

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -69,9 +69,11 @@ class CatalogController < ApplicationController
     config.add_facet_field 'topics', label: 'Topic', solr_params: { 'facet.limit' => -1 },
                                      message: 'Cataloging in progress: These tags do not reflect all AAPB content.'
     config.add_facet_field 'asset_type'
+    config.add_facet_field 'state', solr_params: { 'facet.limit' => -1 },
+                                    show: false, tag: 'state'
     config.add_facet_field 'organization', sort: 'index', solr_params: { 'facet.limit' => -1 },
                            # Default is 100, but we have more orgs than that. -1 means no limit.
-                                           tag: 'org', ex: 'org',
+                                           tag: 'org', ex: 'org,state',
                                            partial: 'organization_facet'
                            # Display all, even when one is selected.
     config.add_facet_field 'year', sort: 'index', range: true,
