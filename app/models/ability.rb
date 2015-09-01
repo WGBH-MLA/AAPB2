6d000e3151afc33cdf1fc63039d9b8775b3560df
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-09-01 17:33:32 -0400

    Turn on the ORR.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,23 +4,24 @@ class Ability
   def initialize(user)
     can :skip_tos, PBCore
 
+    # OLD
+#    can :play, PBCore do |pbcore|
+#      user.onsite? && user.affirmed_tos? && (pbcore.public? || pbcore.protected?)
+#    end
+
     can :play, PBCore do |pbcore|
-      user.onsite? && user.affirmed_tos? && (pbcore.public? || pbcore.protected?)
+      (user.onsite? && (pbcore.public? || pbcore.protected?)) ||
+      (user.usa? && !user.bot? && user.affirmed_tos? && pbcore.public?)
     end
 
-    # TODO:
-#    can :play, PBCore do |pbcore|
-#      (user.onsite? && (pbcore.public? || pbcore.protected?)) ||
-#      (user.usa? && !user.bot? && user.affirmed_tos? && pbcore.public?)
+    # OLD
+#    cannot :skip_tos, PBCore do |pbcore|
+#      user.onsite? && !user.affirmed_tos? && (pbcore.public? || pbcore.protected?)
 #    end
 
     cannot :skip_tos, PBCore do |pbcore|
-      user.onsite? && !user.affirmed_tos? && (pbcore.public? || pbcore.protected?)
+      user.usa? && !user.bot? && !user.affirmed_tos? && pbcore.public?
     end
 
-    # TODO:
-#    cannot :skip_tos, PBCore do |pbcore|
-#      user.usa? && !user.bot? && !user.affirmed_tos? && pbcore.public?
-#    end
   end
 end
