ea91550003c97d63d19e1cc657d1ca459a84a000
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-09-03 15:15:27 -0400

    Helpful error messages from API.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,18 +5,22 @@ class ApiController < ApplicationController
     @solr = Solr.instance.connect
     callback = params.delete('callback')
     rows = [params.delete('rows').to_i, 100].min
-    data = @solr.get('select', params: params.except(:action, :format, :controller).merge({rows: rows}))
+    data = begin
+      @solr.get('select', params: params.except(:action, :format, :controller).merge({rows: rows}))
+    rescue => e
+      # RSolr dictates that responses be ruby data structures,
+      # but the eval still scares me.
+      eval(e.response[:body])
+    end
     respond_to do |format|
       format.json do
         pretty = JSON.pretty_generate(data)
-        if callback
-          render text: "#{callback}(#{pretty});"
-        else
-          render text: pretty;
-        end
+        render text: (callback ? "#{callback}(#{pretty});" : pretty), 
+               status: data['error'] ? 500 : 200
       end
       format.xml do
-        render text: data.to_xml
+        render text: data.to_xml,
+               status: data['error'] ? 500 : 200
       end
     end
   end
--- spec/features/api_spec.rb
+++ spec/features/api_spec.rb
@@ -8,29 +8,57 @@ describe 'API' do
     PBCoreIngester.load_fixtures
   end
 
-  describe '#index' do
+  describe 'good queries' do
     it 'retrieves individual documents' do
       visit '/api.json?callback=callback&rows=1&q=id:1234&fl=id,title'
+      expect(page.status_code).to eq 200
       expect(page).to have_text('callback({ "responseHeader"')
       expect(page).to have_text('"rows": "1"')
       expect(page).to have_text('Gratuitous Explosions')
     end
 
-    it 'searches documents' do
-      visit '/api.json?callback=callback&rows=10&q=iowa'
-      expect(page).to have_text('callback({ "responseHeader"')
-      expect(page).to have_text('"numFound": 3')
-      expect(page).to have_text('Borlaug')
-    end
-    
     it 'supports facets for statistics' do
       visit '/api.json?callback=callback&facet=true&facet.field=year&' + 
         'facet.query[]=year:1988+AND+iowa'
+      expect(page.status_code).to eq 200
       expect(page).to have_text('callback({ "responseHeader"')
       expect(page).to have_text('"rows": "0"')
       expect(page).to have_text('"year:1988 AND iowa": 1')
       expect(page).to have_text('"numFound": 24')
       expect(page).to have_text('"1981", 1, "1988", 1, "1990", 1, "2000", 1')
+    end
+    
+    it 'searches documents' do
+      visit '/api.json?callback=callback&rows=10&q=iowa'
+      expect(page.status_code).to eq 200
+      expect(page).to have_text('callback({ "responseHeader"')
+      expect(page).to have_text('"numFound": 3')
+      expect(page).to have_text('Norman Borlaug')
+      expect(page.source).to match('"xml": "<pbcoreDescriptionDocument')
+      # have_text runs the source through a regex that removes "tags",
+      # even in non-xml documents.
+    end
+    
+    it 'supports xml, too' do
+      visit '/api.xml?rows=10&q=iowa'
+      expect(page.status_code).to eq 200
+      expect(page.source).to match('<numFound type="integer">3</numFound>')
+      expect(page.source).to match('Norman Borlaug')
+      expect(page.source).to match('<xml>&lt;pbcoreDescriptionDocument')
+    end
+  end
+  
+  describe 'error handling' do
+    it 'has helpful error messages in json' do
+      visit '/api.json?facet=error'
+      expect(page.status_code).to eq 500
+      expect(page).to have_text('"msg": "invalid boolean value: error"')
+    end
+    
+    it 'has helpful error messages in xml' do
+      visit '/api.xml?facet=error'
+      expect(page.status_code).to eq 500
+      expect(page.source).to match('<msg>invalid boolean value: error</msg>')
     end
   end
 end
