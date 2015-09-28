f25200395644f6f751dc64b4cf2925bbdc698029
Andrew Myers <afredmyers@gmail.com>
2015-09-28 16:17:20 -0400

    Dry up javascript

--- app/assets/javascripts/custom.jquery.js
+++ app/assets/javascripts/custom.jquery.js
@@ -2,44 +2,26 @@
 
 $(document).ready(function () {
 
-	//check window size and inserts an <hr> appropriately
-	if ($(window).width() < 991) {
-		$("#documents.row .record:nth-of-type(even)").after("<hr>");
-	}
-	else {
-		$("hr").remove();
-		$("#documents.row .record:nth-of-type(3n)").after("<hr>");
-	}
 
-	//Same thing as about but accounts for window resize
+  function doneResizing() {
+    if ($(window).width() < 991) {
+      $("#documents.row hr").remove();
+      $("#documents.row .record:nth-of-type(even)").after("<hr>");
+    }
+    else {
+      $("#documents.row hr").remove();
+      $("#documents.row .record:nth-of-type(3n)").after("<hr>");
+    }
+  }
+
+  doneResizing();
+
 	var resizeId;
 	$(window).resize(function() {
 	  clearTimeout(resizeId);
 	  resizeId = setTimeout(doneResizing, 200);
 	});
 
-	function doneResizing() {
-		if ($(window).width() < 991) {
-			$("hr").remove();
-			$("#documents.row .record:nth-of-type(even)").after("<hr>");
-		}
-		else {
-			$("hr").remove();
-	    $("#documents.row .record:nth-of-type(3n)").after("<hr>");
-		}
-	}
+  $(document).ajaxSuccess(doneResizing)
 
-
-});
-
-// Listen for Button CLicks
-$(document).ajaxSuccess(function() {
-	//check window size and inserts an <hr> appropriately
-	if ($(window).width() < 991) {
-		$("#documents.row .record:nth-of-type(even)").after("<hr>");
-	}
-	else {
-		$("hr").remove();
-		$("#documents.row .record:nth-of-type(3n)").after("<hr>");
-	}
 });
