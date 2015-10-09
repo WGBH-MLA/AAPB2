0a016d54315f2f50d39218ac3ed971b6813b6fd8
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-10-09 12:00:30 -0400

    Non-ascii in SRT confuses this, so skip.

--- spec/support/code_style_spec.rb
+++ spec/support/code_style_spec.rb
@@ -6,7 +6,7 @@ describe 'code style' do
 
     all_paths = Dir[Rails.root + '{app,config,scripts,spec}/**/*']
     paths_to_check = all_paths.reject do |path|
-      path =~ /\.(jar|ico|png|gif|jpg|dat)$/ ||
+      path =~ /\.(jar|ico|png|gif|jpg|dat|srt)$/ ||
       File.directory?(path) ||
       path == __FILE__
     end
