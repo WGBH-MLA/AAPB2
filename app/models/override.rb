8cb8295055d53f6249bab36475898a07e5988152
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-10-30 18:06:14 -0400

    Pull-left every image. Fix #746.

--- app/models/override.rb
+++ app/models/override.rb
@@ -1,4 +1,13 @@
 class Override < Cmless
   ROOT = (Rails.root + 'app/views/override').to_s
   attr_reader :body_html
+  def body_html_wrapped
+    @body_html_wrapped ||= begin
+      noko = Nokogiri::HTML::DocumentFragment.parse(body_html)
+      noko.xpath('.//img').each do |img|
+        img['class'] = [img['class'], ' pull-left'].join
+      end
+      noko.to_html
+    end
+  end
 end
