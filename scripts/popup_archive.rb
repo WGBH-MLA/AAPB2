df63533a444f898cb7a0ab8f15a38ea4a5ee603e
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-04-26 15:32:00 -0400

    Good enough for now?

--- scripts/popup_archive.rb
+++ scripts/popup_archive.rb
@@ -33,11 +33,17 @@ client.list_collections.collections.each do |collection|
   Dir.mkdir(subdir)
   
   collection.item_ids.each do |item_id|
-    puts item_id
-    entities = client.get_item(collection.id, item_id).entities
+    item = client.get_item(collection.id, item_id)
+    puts "#{item.title}: #{item.id}"
+    
+    base = item.title.sub(/.*\//, '')
     File.write(
-      File.join(subdir, "#{item_id}-entities.json"), 
-      JSON.pretty_generate(entities))
+      File.join(subdir, "#{base}-entities.json"), 
+      JSON.pretty_generate(item.entities))
+    fail("Expected exactly one audio file per item") unless item.audio_files.size == 1
+    File.write(
+      File.join(subdir, "#{base}-transcript.json"), 
+      JSON.pretty_generate(item.audio_files.first.transcript))
   end
   
   puts
