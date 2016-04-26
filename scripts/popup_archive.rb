3cd65f2a18792522129270afe786a81ec1da9880
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-04-26 14:58:50 -0400

    lists the entities as json files.

--- scripts/popup_archive.rb
+++ scripts/popup_archive.rb
@@ -1,13 +1,21 @@
 require 'popuparchive'
+require 'json'
+
+fail('Expects one argument') unless ARGV.length == 1
+dir = ARGV.shift
+fail('Expects argument not to exist yet') if File::exists?(dir)
+Dir.mkdir(dir)
 
 creds = YAML.load_file(__dir__ + '/popup-oauth.yml')
 
 module PopUpArchive
   class Client
     def list_collections
-      resp = get('/collections')
-      return resp.http_resp.body
+      get('/collections').http_resp.body
     end
+#    def pp(s)
+#      puts s
+#    end
   end
 end
 
@@ -15,7 +23,22 @@ client = PopUpArchive::Client.new(
   id: creds['id'],
   secret: creds['secret'],
   host: 'https://www.popuparchive.com/',
-  debug: false
+#  debug: true
 )
 
-puts client.list_collections.to_json
+client.list_collections.collections.each do |collection|
+  puts "#{collection.title}: #{collection.id}"
+  
+  subdir = File.join(dir, collection.id.to_s)
+  Dir.mkdir(subdir)
+  
+  collection.item_ids.each do |item_id|
+    puts item_id
+    entities = client.get_item(collection.id, item_id).entities
+    File.write(
+      File.join(subdir, "#{item_id}-entities.json"), 
+      JSON.pretty_generate(entities))
+  end
+  
+  puts
+end
