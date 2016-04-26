bc36ca50f695f364092de8cf82b201a3f76750e5
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-04-26 15:48:02 -0400

    Style

--- scripts/popup_archive.rb
+++ scripts/popup_archive.rb
@@ -1,9 +1,9 @@
 require 'popuparchive'
 require 'json'
 
-fail('Expects one argument') unless ARGV.length == 1
+raise('Expects one argument') unless ARGV.length == 1
 dir = ARGV.shift
-fail('Expects argument not to exist yet') if File::exists?(dir)
+raise('Expects argument not to exist yet') if File.exist?(dir)
 Dir.mkdir(dir)
 
 creds = YAML.load_file(__dir__ + '/popup-oauth.yml')
@@ -13,9 +13,9 @@ module PopUpArchive
     def list_collections
       get('/collections').http_resp.body
     end
-#    def pp(s)
-#      puts s
-#    end
+    #    def pp(s)
+    #      puts s
+    #    end
   end
 end
 
@@ -28,23 +28,23 @@ client = PopUpArchive::Client.new(
 
 client.list_collections.collections.each do |collection|
   puts "#{collection.title}: #{collection.id}"
-  
+
   subdir = File.join(dir, collection.id.to_s)
   Dir.mkdir(subdir)
-  
+
   collection.item_ids.each do |item_id|
     item = client.get_item(collection.id, item_id)
     puts "#{item.title}: #{item.id}"
-    
-    base = item.title.sub(/.*\//, '')
+
+    base = item.title.sub(%r{.*/}, '')
     File.write(
-      File.join(subdir, "#{base}-entities.json"), 
+      File.join(subdir, "#{base}-entities.json"),
       JSON.pretty_generate(item.entities))
-    fail("Expected exactly one audio file per item") unless item.audio_files.size == 1
+    raise('Expected exactly one audio file per item') unless item.audio_files.size == 1
     File.write(
-      File.join(subdir, "#{base}-transcript.json"), 
+      File.join(subdir, "#{base}-transcript.json"),
       JSON.pretty_generate(item.audio_files.first.transcript))
   end
-  
+
   puts
 end
