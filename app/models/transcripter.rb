d29e356e574503c1ef34f996ca95228fd005013c
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-05-23 10:30:51 -0400

    transcriptor spec passes.

--- app/models/transcripter.rb
+++ app/models/transcripter.rb
@@ -1,5 +1,6 @@
 require 'srt'
 require 'nokogiri'
+require 'time'
 
 class Transcripter
   def self.from_srt(srt)
@@ -7,12 +8,17 @@ class Transcripter
     Nokogiri::XML::Builder.new do |x|
       x.div(class: 'transcript') {
         transcript.lines.each { |line|
-          x.div('data-timecodebegin' => line.start_time, 'data-timecodeend' => line.end_time) {
+          x.div(
+            'data-timecodebegin' => Transcripter.as_timestamp(line.start_time),'data-timecodeend' => Transcripter.as_timestamp(line.end_time)) {
             x.span(class: 'play-from-here', 'data-timecodebegin' => line.start_time)
             x.text(line.text.join("\n"))
           }
         }
       }
-    end.to_xml.gsub('<?xml version="1.0"?>', '')
+    end.to_xml.gsub("<?xml version=\"1.0\"?>\n", '')
+  end
+  
+  def self.as_timestamp(s)
+    Time.at(s).utc.strftime("%H:%M:%S.%L")
   end
 end
