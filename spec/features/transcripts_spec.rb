c7b35d31de1937e54b3a8d7b30975e16eeaa73a2
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-05-23 12:00:26 -0400

    Fix fixture and style.

--- spec/features/transcripts_spec.rb
+++ spec/features/transcripts_spec.rb
@@ -2,14 +2,13 @@ require 'rails_helper'
 require_relative '../../scripts/lib/pb_core_ingester'
 
 describe 'Transcripts' do
-#  include ValidationHelper
+  #  include ValidationHelper
 
   before(:all) do
     PBCoreIngester.load_fixtures
   end
 
   describe '#show' do
-
     it 'renders SRT as HTML' do
       visit '/transcripts/1234'
       expect(page).to have_text('Raw bytes 0-255 follow')
