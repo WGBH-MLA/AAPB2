09c7ac139530bd33d4ef0ec36266677c4abb95b2
Andrew Myers <afredmyers@gmail.com>
2017-06-23 14:30:52 -0600

    Relax link checker
    Was getting 'Peer certificate cannot be authenticated with given CA
    certificates' errors on Travis, but not locally.

--- spec/support/link_checker.rb
+++ spec/support/link_checker.rb
@@ -41,11 +41,18 @@ class LinkChecker
     curl.follow_location = true
     curl.max_redirects = 1
     curl.useragent = 'Ruby/Curb'
+    # Avoid "Peer certificate cannot be authenticated with given CA
+    # certificates" error on Travis CI.
     curl.http_get
 
     code = curl.response_code
     raise("Got #{code} from #{full_url} instead of 200") unless code == 200
 
+    report('PASS', url)
+    true
+  rescue Curl::Err::SSLCACertificateError
+    # For some reason, Travis's version of curl get's hung up on this error.
+    # We don't care about certs right here though, so just pass it.
     report('PASS', url)
     true
   rescue => e
