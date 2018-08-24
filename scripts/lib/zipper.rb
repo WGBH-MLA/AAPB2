3988f539380ea0b13620e69be78454a92693ae53
fog <henry_neels@wgbh.org>
2018-08-24 15:43:21 -0400

    'no offense'

--- scripts/lib/zipper.rb
+++ scripts/lib/zipper.rb
@@ -2,32 +2,29 @@ require 'zip'
 
 module Zipper
   def self.write(filename, content)
-
     Zip::File.open(filename + '.zip', Zip::File::CREATE) do |zipfile|
       zipfile.get_output_stream(File.basename(filename)) { |stream| stream << content }
     end
   end
 
   def self.read(path)
-
     filename = path.to_s
 
     str = if filename =~ /\.zip$/
 
-      # zipper
-      Zip::File.open(filename, Zip::File::CREATE) do |z|
-        str = z.read( File.basename( filename.gsub(/\.zip$/, '') ) )
-        z.close
-      end
-
-      str
-    else
-      # not zip
-      f = File.open(filename)
-      str = f.read
-      f.close
-      str
-    end
+            # zipper
+            Zip::File.open(filename, Zip::File::CREATE) do |z|
+              s = z.read(File.basename(filename.gsub(/\.zip$/, '')))
+              z.close
+              s
+            end
+          else
+            # not zip
+            f = File.open(filename)
+            s = f.read
+            f.close
+            s
+          end
 
     unless filename.include?('spec/fixtures') || filename.include?('spec/scripts')
       File.delete(filename)
@@ -35,5 +32,4 @@ module Zipper
 
     str
   end
-
 end
