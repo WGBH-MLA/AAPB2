c8d7588c3f795fd0229443b7dfa3cf439f7b1bf9
fog <henry_neels@wgbh.org>
2018-09-10 17:15:38 -0400

    'fix defaults'

--- app/controllers/exhibits_controller.rb
+++ app/controllers/exhibits_controller.rb
@@ -7,7 +7,7 @@ class ExhibitsController < OverrideController
   end
 
   def show
-    @exhibit = Exhibit.new(params[:path])
+    @exhibit = Exhibit.find_by_path(params[:path])
     @page_title = @exhibit.title
     params[:path] = nil
   end
