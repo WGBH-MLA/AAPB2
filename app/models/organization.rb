f9a5532dd4087a638911ec91c62f36e53251f0be
foglabs <foglaboratories@gmail.com>
2018-10-23 11:34:48 -0400

    'remove org sidebar, rename history section/title to about'

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -11,7 +11,7 @@ class Organization < Cmless
   attr_reader :state_html
   attr_reader :city_html
   attr_reader :url_html
-  attr_reader :history_html
+  attr_reader :about_html
   attr_reader :productions_html
   attr_reader :logo_html
 
@@ -28,7 +28,7 @@ class Organization < Cmless
   end
 
   def summary
-    @summary ||= history_html.sub(/(^.{10,}?\.\s+)([A-Z].*)?/m, '\1')
+    @summary ||= about_html.sub(/(^.{10,}?\.\s+)([A-Z].*)?/m, '\1')
   end
 
   def logo_src
