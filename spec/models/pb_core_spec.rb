19d9db9b41f01c5d14307b10026227ffece866ca
foglabs <foglaboratories@gmail.com>
2018-11-16 10:24:06 -0500

    'remove merge stuff'

--- spec/models/pb_core_spec.rb
+++ spec/models/pb_core_spec.rb
@@ -215,20 +215,12 @@ describe 'Validated and plain PBCore' do
         contributing_organization_objects: [Organization.find_by_pbcore_name('WGBH')],
         states: ['Massachusetts'],
         img?: true,
-<<<<<<< HEAD
-        all_parties: [PBCoreNameRoleAffiliation.new('contributor', 'Curly', 'bald', 'Stooges'),
-                      PBCoreNameRoleAffiliation.new('creator', 'Larry', 'balding', 'Stooges'),
-                      PBCoreNameRoleAffiliation.new('creator', 'WGBH', 'Producing Organization', 'Stooges'),
-                      PBCoreNameRoleAffiliation.new('publisher', 'Moe', 'hair', 'Stooges')
-                    ]
-=======
         all_parties: [
           PBCoreNameRoleAffiliation.new('creator', 'WGBH', 'Producing Organization', 'Stooges'),
           PBCoreNameRoleAffiliation.new('contributor', 'Curly', 'bald', 'Stooges'),
           PBCoreNameRoleAffiliation.new('creator', 'Larry', 'balding', 'Stooges'),
           PBCoreNameRoleAffiliation.new('publisher', 'Moe', 'hair', 'Stooges')
         ]
->>>>>>> master
       }
 
       pbc = PBCore.new(pbc_xml)
