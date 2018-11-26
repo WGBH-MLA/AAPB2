6ec9d0b30be64022bf64cb167c72b81b53202f7b
foglabs <foglaboratories@gmail.com>
2018-11-26 16:59:39 -0500

    'be right back'

--- app/controllers/advanced_controller.rb
+++ app/controllers/advanced_controller.rb
@@ -2,7 +2,18 @@ require 'cgi'
 
 class AdvancedController < ApplicationController
   def create
-    redirect_to "/catalog?q=#{CGI.escape(query)}"
+    # fqoo = if !params[:exact].empty?
+    #   # "&fq=" + CGI.escape(%((captions_unstemmed:+"#{params[:exact]}" OR text_unstemmed:+"#{params[:exact]}" OR titles_unstemmed:+"#{params[:exact]}" OR contribs_unstemmed:+"#{params[:exact]}" OR title_unstemmed:+"#{params[:exact]}" OR contributing_organizations_unstemmed:+"#{params[:exact]}" OR producing_organizations_unstemmed:+"#{params[:exact]}" OR genres_unstemmed:+"#{params[:exact]}" OR topics_unstemmed:+"#{params[:exact]}")))
+    # else
+    #   ""
+    # end
+    qoo = if !params[:exact].empty?
+      "q=" + CGI.escape(%(captions_unstemmed:+"#{params[:exact]}" OR text_unstemmed:+"#{params[:exact]}" OR titles_unstemmed:+"#{params[:exact]}" OR contribs_unstemmed:+"#{params[:exact]}" OR title_unstemmed:+"#{params[:exact]}" OR contributing_organizations_unstemmed:+"#{params[:exact]}" OR producing_organizations_unstemmed:+"#{params[:exact]}" OR genres_unstemmed:+"#{params[:exact]}" OR topics_unstemmed:+"#{params[:exact]}"))
+    else
+      "q=#{CGI.escape(query)}"
+    end
+
+    redirect_to "/catalog?#{qoo}"
   end
 
   def query
