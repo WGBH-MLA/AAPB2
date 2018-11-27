21e3540730ba5a5317cd7bb0d669f13316da2850
foglabs <foglaboratories@gmail.com>
2018-11-27 11:07:58 -0500

    'yeesh'

--- app/controllers/advanced_controller.rb
+++ app/controllers/advanced_controller.rb
@@ -7,17 +7,15 @@ class AdvancedController < ApplicationController
     # else
     #   ""
     # end
-    qoo = if !params[:exact].empty?
-      "q=" + CGI.escape(%(captions_unstemmed:+"#{params[:exact]}" OR text_unstemmed:+"#{params[:exact]}" OR titles_unstemmed:+"#{params[:exact]}" OR contribs_unstemmed:+"#{params[:exact]}" OR title_unstemmed:+"#{params[:exact]}" OR contributing_organizations_unstemmed:+"#{params[:exact]}" OR producing_organizations_unstemmed:+"#{params[:exact]}" OR genres_unstemmed:+"#{params[:exact]}" OR topics_unstemmed:+"#{params[:exact]}"))
-    else
-      "q=#{CGI.escape(query)}"
-    end
-
+    # qoo = "q=#{CGI.escape(query)}"
+    qoo = "q=#{query}"
     redirect_to "/catalog?#{qoo}"
   end
 
   def query
-    [
+    fieldnames = ['captions_unstemmed','text_unstemmed','titles_unstemmed','contribs_unstemmed','title_unstemmed','contributing_organizations_unstemmed','producing_organizations_unstemmed','genres_unstemmed','topics_unstemmed']
+
+    qooqoo = [
       !params[:all].empty? &&
         self.class.prefix(params[:all], '+'),
 
@@ -25,7 +23,7 @@ class AdvancedController < ApplicationController
         "+titles:\"#{params[:title]}\"",
 
       !params[:exact].empty? &&
-        "+\"#{params[:exact]}\"",
+        fieldnames.map.with_index {|fieldname, i| %(#{fieldname}:+"#{params[:exact]}"#{i != (fieldnames.count-1) ? ' OR ' : nil})}.join,
 
       !params[:any].empty? &&
         self.class.prefix(params[:any], '', ' OR '),
@@ -34,6 +32,8 @@ class AdvancedController < ApplicationController
         self.class.prefix(params[:none], '-')
 
     ].select { |clause| clause }.join(' ')
+
+    %((#{qooqoo}))
   end
 
   def self.prefix(terms, prefix, joint = ' ')
