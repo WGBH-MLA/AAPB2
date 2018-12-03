c3369126c3964a8059a61968c218979499c71abc
foglabs <foglaboratories@gmail.com>
2018-12-03 15:25:06 -0500

    'change assertions in advancedcon spsec'

--- spec/controllers/advanced_controller_spec.rb
+++ spec/controllers/advanced_controller_spec.rb
@@ -5,11 +5,11 @@ describe AdvancedController do
     assertions = [
       [{ all: 'all of these' }, '+all +of +these'],
       [{ title: 'some title' }, '+titles:"some title"'],
-      [{ exact: 'exactly these' }, '+(captions_unstemmed:"exactly these" OR text_unstemmed:"exactly these" OR titles_unstemmed:"exactly these" OR contribs_unstemmed:"exactly these" OR title_unstemmed:"exactly these" OR contributing_organizations_unstemmed:"exactly these" OR producing_organizations_unstemmed:"exactly these" OR genres_unstemmed:"exactly these" OR topics_unstemmed:"exactly these")'],
+      [{ exact: 'exactly these' }, '+(captions_unstemmed:"exactly these" OR text_unstemmed:"exactly these" OR titles_unstemmed:"exactly these" OR contribs_unstemmed:"exactly these" OR title_unstemmed:"exactly these" OR contributing_organizations_unstemmed:"exactly these" OR producing_organizations_unstemmed:"exactly these" OR genres_unstemmed:"exactly these" OR topics_unstemmed:"exactly these") '],
       [{ any: 'any of these' }, 'any OR of OR these'],
       [{ none: 'none of these' }, '-none -of -these'],
       [{ all: 'all', title: 'title', exact: 'exact', any: 'any', none: 'none' },
-       '+all +titles:"title" +"exact" any -none']
+       '+(captions_unstemmed:"exact" OR text_unstemmed:"exact" OR titles_unstemmed:"exact" OR contribs_unstemmed:"exact" OR title_unstemmed:"exact" OR contributing_organizations_unstemmed:"exact" OR producing_organizations_unstemmed:"exact" OR genres_unstemmed:"exact" OR topics_unstemmed:"exact") +all +titles:"title" any -none']
     ]
     assertions.each do |params, query|
       it "handles #{params}" do
