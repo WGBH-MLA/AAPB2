f8c97a9a4da7c1ef6d4b108e75666586994ee669
foglabs <foglaboratories@gmail.com>
2018-12-03 14:03:04 -0500

    'pass in constraints to advanced search, tests'

--- app/controllers/advanced_controller.rb
+++ app/controllers/advanced_controller.rb
@@ -1,22 +1,24 @@
 require 'cgi'
 
 class AdvancedController < ApplicationController
-  def create
+  def index
+    @hidden_constraints = hidden_constraints
+  end
 
+  def create
     if params[:exact].empty?
       qoo = "#{query}"
     else
       # separate exact clause from rest of query
       qoo = "#{exactquery} #{query}"
     end
-    redirect_to "/catalog?q=#{CGI.escape(qoo)}"
+    redirect_to "/catalog?q=#{CGI.escape(qoo)}#{pass_constraints}"
   end
 
   def exactquery
     # mandatory OR query for each unstemmed field
     fieldnames = ['captions_unstemmed','text_unstemmed','titles_unstemmed','contribs_unstemmed','title_unstemmed','contributing_organizations_unstemmed','producing_organizations_unstemmed','genres_unstemmed','topics_unstemmed']
-    eq=fieldnames.map.with_index {|fieldname, i| %(#{fieldname}:"#{params[:exact]}"#{i != (fieldnames.count-1) ? ' OR ' : nil})}.join
-    %(+(#{eq}))
+    %(+(#{fieldnames.map {|fieldname| %(#{fieldname}:"#{params[:exact]}")}.join(' OR ')}))
   end
 
   def query
@@ -39,4 +41,16 @@ class AdvancedController < ApplicationController
   def self.prefix(terms, prefix, joint = ' ')
     terms.split(/\s+/).map { |term| "#{prefix}#{term}" }.join(joint)
   end
+
+  private
+
+  def hidden_constraints
+    params[:f]
+  end
+
+  def pass_constraints
+    # TODO: whitelist other parameters that we want to carry through advanced search to cat controller
+    params[:f].map {|k,v| %(&f[#{k}][]=#{v})}.join if params[:f]
+  end
+
 end
--- spec/features/advanced_search_spec.rb
+++ spec/features/advanced_search_spec.rb
@@ -11,17 +11,34 @@ describe "Advanced Search Integration" do
     visit "/advanced?f[access_types][]=#{PBCore::ALL_ACCESS}"
   end
 
-  it 'can do an exact search' do
+  it 'matches entire phrase for exact search' do
 
-    fill_in('exact', with: 'Rez')
+    fill_in('exact', with: 'Win or lose')
     find('#advanced-search').click
-    # records dont come up as they do when searching from client?
+    expect(page).to have_text("Racing the Rez")
+    expect(page).to_not have_text("The Lost Year")
+    expect(page).to_not have_text("The Civil War; Interviews with Barbara Fields")
+    expect(page).to have_text("1 entry found")
+  end
+
+  it 'searches with a mix of exact and other search terms' do
+    fill_in('all', with: 'rez')
+    fill_in('title', with: 'racing')
+    fill_in('exact', with: 'runners')
+    fill_in('any', with: 'vision')
+    fill_in('none', with: 'artichoke')
+    find('#advanced-search').click
     expect(page).to have_text("Racing the Rez")
   end
 
-  it 'can do a hybrid search' do
-  end
 
-  it 'can do an all these words search' do
+  it 'enforces none of these words searches' do
+    fill_in('all', with: 'rez')
+    fill_in('exact', with: 'runners')
+    fill_in('any', with: 'vision')
+    fill_in('none', with: 'racing')
+    find('#advanced-search').click
+    expect(page).to_not have_text("Racing the Rez")
+    expect(page).to have_text("No entries found")
   end
 end
