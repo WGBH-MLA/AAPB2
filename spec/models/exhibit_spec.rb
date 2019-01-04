954db146d327d530f2028df99112660ee0c4e633
foglabs <foglaboratories@gmail.com>
2019-01-04 10:20:29 -0500

    'cover test'

--- spec/models/exhibit_spec.rb
+++ spec/models/exhibit_spec.rb
@@ -37,18 +37,8 @@ describe Exhibit do
       resources_html: %(<ul>\n<li><a href=\"http://loc.gov\">LoC</a></li>\n<li><a href=\"http://wgbh.org\">WGBH</a></li>\n</ul>),
       resources: [['LoC', 'http://loc.gov'], ['WGBH', 'http://wgbh.org']],
       main_html: %(<p>Description goes here. Description goes here. Description goes here. Description goes here. Description goes here. Description goes here. Description goes here. Description goes here. Description goes here. Description goes here. Description goes here. Description goes here. Description goes here. Description goes here. Description goes here. Description goes here. Description goes here. Description goes here. Description goes here. Description goes here. Description goes here. Description goes here. Description goes here. Description goes here. \n<a href=\"/catalog/cpb-aacip_80-12893j6c\">item 1</a>\n<a href=\"/catalog/cpb-aacip_37-31cjt2qs\">item 2</a>\n<a href=\"/catalog/cpb-aacip_192-1937pxnq\" title=\"fuller description\">item 3</a></p>),
-      cover: %(<a href='/exhibits/parent/child/grandchild'>
-        <div style="background-image: url('https://s3.amazonaws.com/americanarchive.org/exhibits/AAPB_Exhibit_Newsmagazines_image5.jpg');" class='four-four-box exhibit-section'>
-
-          <div class='exhibit-cover-overlay bg-color-red'></div>
-
-          <div class='exhibit-cover-text'>
-            Grandchild!
-          </div>
-        </div>
-      </a>),
-
       cover_html: %(<p><img title=\"cover title 2\" alt=\"Alt cover 2\" src=\"https://s3.amazonaws.com/americanarchive.org/exhibits/AAPB_Exhibit_Newsmagazines_image5.jpg\"></p>),
+      cover: nil, #needs to account for random bgcolor
       gallery: [{ credit_url: 'http://www.cpb.org/link1',
                   asset_url: 'http://americanarchive.org/whoo1',
                   source_text: 'First Source name',
@@ -91,11 +81,49 @@ describe Exhibit do
 
     assertions.each do |method, value|
       it "\##{method} method works" do
-        expect(exhibit.send(method)).to eq((begin
-                                              value.strip
-                                            rescue
-                                              value
-                                            end))
+
+        # method uses random bgcolor, 
+        if method == :cover
+
+          expect([%(<a href='/exhibits/parent/child/grandchild'>
+        <div style="background-image: url('https://s3.amazonaws.com/americanarchive.org/exhibits/AAPB_Exhibit_Newsmagazines_image5.jpg');" class='four-four-box exhibit-section'>
+
+          <div class='exhibit-cover-overlay bg-color-red'></div>
+
+          <div class='exhibit-cover-text'>
+            Grandchild!
+          </div>
+        </div>
+      </a>),
+          %(<a href='/exhibits/parent/child/grandchild'>
+        <div style="background-image: url('https://s3.amazonaws.com/americanarchive.org/exhibits/AAPB_Exhibit_Newsmagazines_image5.jpg');" class='four-four-box exhibit-section'>
+
+          <div class='exhibit-cover-overlay bg-color-pink'></div>
+
+          <div class='exhibit-cover-text'>
+            Grandchild!
+          </div>
+        </div>
+      </a>),
+          %(<a href='/exhibits/parent/child/grandchild'>
+        <div style="background-image: url('https://s3.amazonaws.com/americanarchive.org/exhibits/AAPB_Exhibit_Newsmagazines_image5.jpg');" class='four-four-box exhibit-section'>
+
+          <div class='exhibit-cover-overlay bg-color-purple'></div>
+
+          <div class='exhibit-cover-text'>
+            Grandchild!
+          </div>
+        </div>
+      </a>)].include?(exhibit.send(method))).to eq(true)
+
+        else
+
+          expect(exhibit.send(method)).to eq((begin
+                                                value.strip
+                                              rescue
+                                                value
+                                              end))          
+        end
       end
     end
 
