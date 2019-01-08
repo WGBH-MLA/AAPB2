945677b76756a993e492a101b6a9cd60fb33183e
foglabs <foglaboratories@gmail.com>
2019-01-08 10:45:17 -0500

    'refine that dang regex'

--- app/helpers/snippet_helper.rb
+++ app/helpers/snippet_helper.rb
@@ -1,8 +1,9 @@
 module SnippetHelper
   def snippet_from_query(query, text, snippet_length, separator)
     return nil unless text
-    text = text.upcase.gsub(/[[:punct:]]/, '')
-    text_dictionary = text.split
+    # text = text.upcase.gsub(/[[:punct:]]/, '')
+    text = text.upcase.gsub(/[^a-zA-z0-9\ \.\,:;!]/, '')
+    text_dictionary = text.gsub(/[[:punct:]]/, '').split
     intersection = query & text_dictionary
     return nil unless intersection && intersection.present?
     intersection_index = text.index(/\b(?:#{intersection[0]})\b/)
