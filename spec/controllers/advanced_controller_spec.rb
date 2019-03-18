10f370a3e12a51fbec00d11283137f1b7e1cb0a7
foglabs <foglaboratories@gmail.com>
2019-03-18 15:39:03 -0400

    'different advanced search redirect expectation'

--- spec/controllers/advanced_controller_spec.rb
+++ spec/controllers/advanced_controller_spec.rb
@@ -6,12 +6,12 @@ describe AdvancedController do
       [{ all: 'all of these' }, '+all +of +these'],
       [{ title: 'some title' }, '+titles:"some title"'],
       # rubocop:disable LineLength
-      [{ exact: 'exactly these' }, '+(captions_unstemmed:"exactly these" OR text_unstemmed:"exactly these" OR titles_unstemmed:"exactly these" OR contribs_unstemmed:"exactly these" OR title_unstemmed:"exactly these" OR contributing_organizations_unstemmed:"exactly these" OR producing_organizations_unstemmed:"exactly these" OR genres_unstemmed:"exactly these" OR topics_unstemmed:"exactly these") '],
+      [{ exact: 'exactly these' }, '"exactly these"'],
       # rubocop:enable LineLength
       [{ any: 'any of these' }, 'any OR of OR these'],
       [{ none: 'none of these' }, '-none -of -these'],
       # rubocop:disable LineLength
-      [{ all: 'all', title: 'title', exact: 'exact', any: 'any', none: 'none' }, '+(captions_unstemmed:"exact" OR text_unstemmed:"exact" OR titles_unstemmed:"exact" OR contribs_unstemmed:"exact" OR title_unstemmed:"exact" OR contributing_organizations_unstemmed:"exact" OR producing_organizations_unstemmed:"exact" OR genres_unstemmed:"exact" OR topics_unstemmed:"exact") +all +titles:"title" any -none']
+      [{ all: 'all', title: 'title', exact: 'exact', any: 'any', none: 'none' }, '+all +titles:"title" any -none "exact"']
       # rubocop:enable LineLength
     ]
     assertions.each do |params, query|
