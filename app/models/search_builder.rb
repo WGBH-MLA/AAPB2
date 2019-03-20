24755ee87312599b2e45580b84eebec0cd038a5b
foglabs <foglaboratories@gmail.com>
2019-03-20 16:06:20 -0400

    'move fq-hack to search_builder'

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -29,4 +29,41 @@ class SearchBuilder < Blacklight::SearchBuilder
                   )
     %(+(#{fieldnames.map { |fieldname| %(#{fieldname}:"#{string}") }.join(' OR ')}))
   end
+
+
+  # monkeypatch copy
+  def facet_value_to_fq_string(facet_field, value)
+      facet_config = blacklight_config.facet_fields[facet_field]
+
+      local_params = []
+      local_params << "tag=#{facet_config.tag}" if facet_config && facet_config.tag
+
+      prefix = ''
+      prefix = "{!#{local_params.join(' ')}}" unless local_params.empty?
+
+      fq = case
+           when (facet_config && facet_config.query)
+             facet_config.query[value][:fq]
+           when (facet_config && facet_config.date)
+             # in solr 3.2+, this could be replaced by a !term query
+             "#{prefix}#{facet_field}:#{RSolr.solr_escape(value)}"
+           when (value.is_a?(DateTime) || value.is_a?(Date) || value.is_a?(Time))
+             "#{prefix}#{facet_field}:#{RSolr.solr_escape(value.to_time.utc.strftime('%Y-%m-%dT%H:%M:%SZ'))}"
+           when (value.is_a?(TrueClass) || value.is_a?(FalseClass) || value == 'true' || value == 'false'),
+             (value.is_a?(Integer) || (value.to_i.to_s == value if value.respond_to? :to_i)),
+             (value.is_a?(Float) || (value.to_f.to_s == value if value.respond_to? :to_f))
+             "#{prefix}#{facet_field}:#{RSolr.solr_escape(value.to_s)}"
+           when value.is_a?(Range)
+             "#{prefix}#{facet_field}:[#{value.first} TO #{value.last}]"
+           else
+             # Before monkey-patching:
+             # "{!raw f=#{facet_field}#{(" " + local_params.join(" ")) unless local_params.empty?}}#{value}"
+             # Do all the extra parts matter?
+             # BEGIN patch replace
+             prefix + value.split(AAPB::QUERY_OR).map { |single| "#{facet_field}:\"#{single}\"" }.join(' ')
+        # END patch
+      end
+    end
+
+
 end
