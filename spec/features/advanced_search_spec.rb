d97c2e2a595b9a4d13110faf745019db0b436f54
foglabs <foglaboratories@gmail.com>
2019-03-21 13:52:58 -0400

    'all access pass to the hottest locales... on earth'

--- spec/features/advanced_search_spec.rb
+++ spec/features/advanced_search_spec.rb
@@ -8,7 +8,7 @@ describe 'Advanced Search Integration' do
   end
 
   before(:each) do
-    visit '/advanced'
+    visit '/advanced?f[access_types][]=all'
   end
 
   it 'matches entire phrase for exact search' do
