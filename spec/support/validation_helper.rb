a9871bf2586a89ec5b6e063936b6f42c065f0fcc
foglabs <foglaboratories@gmail.com>
2019-04-18 16:17:21 -0400

    'reenable linkchecker'

--- spec/support/validation_helper.rb
+++ spec/support/validation_helper.rb
@@ -23,9 +23,7 @@ END
   /x
 
   def expect_fuzzy_xml(options = {})
-
-    return true # :)
-
+    # return true # :)
     allow_default_title = options.delete(:allow_default_title)
 
     # Kludge valid HTML5 to make it into valid XML.
