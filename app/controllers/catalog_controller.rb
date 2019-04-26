39eade98f7e93d0d2a0412386f5505afc338a515
foglabs <foglaboratories@gmail.com>
2019-04-26 15:16:54 -0400

    'use asset_date field for time sorting'

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -177,8 +177,8 @@ class CatalogController < ApplicationController
     # whether the sort is ascending or descending (it must be asc or desc
     # except in the relevancy case).
     config.add_sort_field 'score desc', label: 'relevance'
-    config.add_sort_field 'timestamp desc', label: 'year (newest)'
-    config.add_sort_field 'timestamp asc', label: 'year (oldest)'
+    config.add_sort_field 'asset_date desc', label: 'date (newest)'
+    config.add_sort_field 'asset_date asc', label: 'date (oldest)'
     config.add_sort_field 'title asc', label: 'title'
 
     # If there are more than this many search results, no spelling ("did you
