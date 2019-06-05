89857e4768dfb4d0f23295d233b7d4ba9ced2d9b
foglabs <foglaboratories@gmail.com>
2019-06-05 16:22:03 -0400

    'delete script'

---
+++ scripts/delete_records.rb
@@ -0,0 +1,15 @@
+require_relative '../lib/rails_stub'
+require_relative '../app/models/exhibit'
+require_relative 'lib/downloader'
+require_relative 'lib/cleaner'
+require_relative 'lib/pb_core_ingester'
+require 'logger'
+require 'rake'
+
+args = ARGV
+ingester = PBCoreIngester.new
+# unless args.all? {|id| /\Acpb-aacip[\-_\/][0-9]{1,3}[\-_\/][a-zA-Z0-9]+\z/ =~ id }
+#   puts "Got a non-GUID argument! That's not cool!"
+#   exit 1 
+# end
+ingester.delete_records(args)
