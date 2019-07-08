a87542684465d72ce3992eff66d4b1719802c61c
Jason Corum <jason.corum@gmail.com>
2019-07-08 07:52:32 -0700

    Rspec outputs (#1771)
    * 'few verbosity fixes'
    
    * 'rubo style'
    
    * 'getting my rubo on'
    
    * 'almost clean'
    
    * Adds error override method
    
    * 'featured method should not have been privvate'
    
    * 'blessed rubo'
    
    * Uses custom feature error
    
    * Rubocop fixes

--- spec/features/transcripts_spec.rb
+++ spec/features/transcripts_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 require_relative '../../scripts/lib/pb_core_ingester'
+require_relative '../support/feature_test_helper'
 
 describe 'Transcripts' do
   #  include ValidationHelper
@@ -12,7 +13,7 @@ describe 'Transcripts' do
   describe '#show' do
     xit 'renders SRT as HTML' do
       visit '/transcripts/1234'
-      expect(page).to have_text('Raw bytes 0-255 follow')
+      expect(page).to have_text('Raw bytes 0-255 follow'), missing_page_text_custom_error('Raw bytes 0-255 follow', page.current_path)
     end
   end
 end
---
+++ spec/support/feature_test_helper.rb
@@ -0,0 +1,13 @@
+module FeatureTestHelper
+  def missing_page_text_custom_error(text, page_path)
+    "expected to find \"#{text}\" on: #{page_path}"
+  end
+
+  def found_page_text_custom_error(text, page_path)
+    "did not expected to find \"#{text}\" on: #{page_path}"
+  end
+end
+
+RSpec.configure do |c|
+  c.include FeatureTestHelper
+end
