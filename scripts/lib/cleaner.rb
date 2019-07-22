00f2d9e02fc38aaacedd9321ab7205b29faf7830
Jason Corum <jason.corum@gmail.com>
2019-07-22 14:15:14 -0700

    Upcases abbreviations like U.S.

--- scripts/lib/cleaner.rb
+++ scripts/lib/cleaner.rb
@@ -236,10 +236,13 @@ class Cleaner
 
       formatted_words = words.map do |word|
         # does allcaps include exact capword, OR does capword appear in word surrounded by word boundary or hyphen OR has no consonants
-        if allcaps.include?(word) || allcaps.any? { |capword| /(\b|-|\\|\/\\)#{capword}(\b|-|\\|\/\\)/ =~ word } || /\b[^AEIOUY]+\b/i =~ word
+        if allcaps.include?(word) || allcaps.any? { |capword| /(\b|-|\\|\/\\)#{capword}(\b|-|\\|\/\\)/ =~ word } || /^[AEIOUY]+$/i =~ word
           word
         elsif nocaps.include?(word)
           word.downcase
+        # does the word contain an abbreviation with periods like U.S.? make all caps
+        elsif  /(?:[a-zA-Z]\.){2,}/i =~ word
+          word.upcase
         else
           word.capitalize
         end
