d7e0bb56711d95ac17b26ea2b5b4e55e84e7f544
foglabs <foglaboratories@gmail.com>
2019-07-23 16:44:19 -0400

    'user access factorires'

--- spec/factories/pbcore_xml/annotation.rb
+++ spec/factories/pbcore_xml/annotation.rb
@@ -7,14 +7,14 @@ FactoryBot.define do
     value { Faker::Quote.famous_last_words }
 
     trait :level_of_user_access do
-      type { 'Level Of User Access' }
+      type { 'Level of User Access' }
     end
     trait :on_location do
       value { 'On Location' }
     end
 
     trait :online_reading_room do
-      value { 'On Location' }
+      value { 'Online Reading Room' }
     end
 
     initialize_with { new(attributes) }
--- spec/support/pb_helpers.rb
+++ spec/support/pb_helpers.rb
@@ -1,5 +1,5 @@
 module PBHelpers
   def new_pb(factory)
-    PBCorePresenter.new(factory.to_xml)
+    PBCorePresenter.new(factory.to_xml.gsub("<?xml version=\"1.0\"?>\n", ''))
   end
 end
