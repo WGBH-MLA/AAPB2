a85c7ec16bb8ccb2cefab2a4a614e74c95ffdb25
foglabs <foglaboratories@gmail.com>
2019-08-30 11:32:02 -0400

    'an incredible solution'

--- lib/html_scrubber.rb
+++ lib/html_scrubber.rb
@@ -16,31 +16,19 @@ class HtmlScrubber
       .gsub(/float: \w+/, '')
       .gsub(' ;', '')
       .gsub(/\d+(pt|px|em)/, '')
+      .split("\n").map(&:strip).join("\n").strip
 
 
     # strip at ends of lines, then replace consecutive whitespace w/ 1 space
-    ActionView::Base.full_sanitizer.sanitize(CGI.unescapeHTML(dirtay)).split("\n").map(&:strip).join("\n").strip
+    if dirtay =~ /\/\w+/
+      # Angle-brackets stripped, so be more aggressive
+      dirtay = dirtay.gsub(/\w+=\S+/, ' ')
+    end
 
+    dirtay = dirtay.gsub(/[ \t]+/, ' ')
+         .gsub(/\n( ?\n)+/, "\n")
+         .gsub(/^\s+|\s+$/, '')
+
+    ActionView::Base.full_sanitizer.sanitize(CGI.unescapeHTML(dirtay))
   end
 end
-
-
-    # # dirty.gsub('&nbsp;', ' ')
-    # #              .gsub('&quot;', '"')
-    #              .gsub(/<style[^>]*>.*?<\/style>/mi, '')
-    #              .gsub(/\{nmap\}.*?\{\/nmap\}/, '')
-    #              .gsub(/<(w:[\S]+).*?\/\1>/m, '') # MS Word special tags with content
-    #              .gsub(/<(p|br)(\s[^>]*)?\/?>/i, "\n")
-    #              .gsub(/<[^>]+>/, '')
-    #              .gsub('\\0x0A', '')
-    #              .gsub(/\?\?\w+/, '')
-    #              .gsub(/\bimg\b/i, '')
-    #              .gsub(/float: \w+/, '')
-    #              .gsub(' ;', '')
-    #              .gsub(/\d+(pt|px|em)/, '')
-
-    #   # Nokogiri::HTML.parse
-
-    #              # .gsub("\n", '')
-
-
