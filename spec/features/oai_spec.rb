dc088e35da7c1280f5d4bad6f407df19cc7d2912
foglabs <foglaboratories@gmail.com>
2019-08-30 13:32:45 -0400

    'publicrecord'

--- spec/features/oai_spec.rb
+++ spec/features/oai_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 require_relative '../../scripts/lib/pb_core_ingester'
 
 describe 'OAI-PMH' do
+
+  before(:all) do
+    @public_xml = just_xml(build(:pbcore_description_document, :full_aapb, access_level_public: true, kqed_org: true, moving_image: true))
+
+    PBCoreIngester.new.delete_all
+    cleaner = Cleaner.instance
+    PBCoreIngester.ingest_record_from_xmlstring(@public_xml)
+    @public_record = PBCorePresenter.new(cleaner.clean(@public_xml))
+  end
   # commenting this out because we don't appear to be using the fxtures in the test.
   # before(:all) do
   #   PBCoreIngester.load_fixtures
@@ -15,7 +24,7 @@ describe 'OAI-PMH' do
       '<OAI-PMH', # Followed by NS
       '<request verb="ListRecords" metadataPrefix="mods">http://openvault.wgbh.org/oai.xml</request>',
       '<ListRecords>',
-      '<identifier type="uri">http://americanarchive.org/catalog/1234</identifier>'
+      %(<identifier type="uri">http://americanarchive.org/catalog/#{@public_record.id}</identifier>)
     ].each do |s|
       expect(page.body).to match s
     end
