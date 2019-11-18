de9804a3f1c46922b8265fc3cd72d0f5e1665848
foglabs <foglaboratories@gmail.com>
2019-11-18 11:07:45 -0500

    'add wrong text to custom error matcher'

--- spec/support/feature_test_helper.rb
+++ spec/support/feature_test_helper.rb
@@ -1,6 +1,11 @@
 module FeatureTestHelper
-  def missing_page_text_custom_error(text, page_path)
-    "expected to find \"#{text}\" on: #{page_path}"
+  def missing_page_text_custom_error(text, page_path, page_text="")
+
+    if page_text.present?
+      page_text = %(, instead got: #{page_text})
+    end
+
+    "expected to find \"#{text}\" on: #{page_path}#{page_text}"
   end
 
   def found_page_text_custom_error(text, page_path)
