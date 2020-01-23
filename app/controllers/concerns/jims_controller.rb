59d528a27fec4e5f3014a9898c681dcec199736f
foglabs <foglaboratories@gmail.com>
2020-01-23 15:41:04 -0500

    'jimdex'

--- app/controllers/concerns/jims_controller.rb
+++ app/controllers/concerns/jims_controller.rb
@@ -1,7 +1,4 @@
 class JimsController < ApplicationController
   def index
-    # load my rekkids
-    @special_collection = @special_collection = SpecialCollection.find_by_path('newshour')
-    render 'jim'
   end
 end
