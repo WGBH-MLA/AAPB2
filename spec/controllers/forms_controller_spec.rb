f169be20077a9ae7d6ed641edf394dbdf4187196
Jason Corum <jason.corum@gmail.com>
2020-03-17 16:38:04 -0700

    Fixes Rubocop errors

--- spec/controllers/forms_controller_spec.rb
+++ spec/controllers/forms_controller_spec.rb
@@ -4,7 +4,6 @@ require 'json'
 require 'webmock'
 
 describe FormsController do
-
   describe '.validate_recaptcha' do
     before :all do
       # WebMock is disabled by default, but we use it for these tests.
@@ -16,14 +15,14 @@ describe FormsController do
       it 'handles successful response' do
         WebMock.stub_request(:post, 'https://www.google.com/recaptcha/api/siteverify').to_return(body: "{\n" + "  \"success\": true,\n" + "  \"challenge_ts\": \"2020-03-10T18:36:28Z\",\n" + "  \"hostname\": \"localhost\",\n" + "  \"score\": 0.9,\n" + "  \"action\": \"subscribe\"\n" + "}")
 
-        post :validate_recaptcha, { "recaptcha_response" => "1234567"}
+        post :validate_recaptcha, "recaptcha_response" => "1234567"
         expect(JSON.parse(response.body)["status"]).to eq(200)
       end
 
       it 'handles unsuccessful response' do
         WebMock.stub_request(:post, 'https://www.google.com/recaptcha/api/siteverify').to_return(body: "{\n" + "  \"success\": false,\n" + "  \"challenge_ts\": \"2020-03-12T18:36:28Z\",\n" + "  \"hostname\": \"localhost\",\n" + "  \"score\": 0.3,\n" + "  \"action\": \"subscribe\"\n" + "}")
 
-        post :validate_recaptcha, { "recaptcha_response" => "1234567"}
+        post :validate_recaptcha, "recaptcha_response" => "1234567"
         expect(JSON.parse(response.body)["message"]).to eq("Submission method not POST or captcha blank")
         expect(JSON.parse(response.body)["status"]).to eq(403)
       end
