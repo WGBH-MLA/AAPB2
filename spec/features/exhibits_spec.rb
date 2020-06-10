b2b42ac07fa96787d167e6dea5a42c0bfd98ff68
Henry Fog <foglaboratories@gmail.com>
2020-06-10 14:52:33 -0400

    VJS Exhibit Gallery Player Fixes (#2068)
    Fixes gallery presentation for media viewing.

--- spec/features/exhibits_spec.rb
+++ spec/features/exhibits_spec.rb
@@ -18,6 +18,6 @@ describe 'Exhibits' do
     # TODO: change this to a real exhibit once they're all reformatted
     visit '/exhibits/civil-rights'
     expect(page.find(:css, 'div.exgal-1 div.exgal-caption')).to have_content 'Warren K. Leffler, photographer. Participants in the March on Washington for Jobs and Freedom, August 28, 1963.'
-    expect(page.find(:css, 'div.exgal-1 h4.exgal-source')).to have_content 'Courtesy of U.S. News & World Report Magazine Photograph Collection, Prints and Photographs Division, Library of Congress.'
+    expect(page.find(:css, 'div.exgal-1 h4.exgal-source_text')).to have_content 'Courtesy of U.S. News & World Report Magazine Photograph Collection, Prints and Photographs Division, Library of Congress.'
   end
 end
