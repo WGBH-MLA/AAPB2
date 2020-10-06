edc91ae09122cb69b0add79de7b3a6571533b57f
Jason Corum <jason.corum@gmail.com>
2020-10-06 09:07:26 -0700

    Adds Volunteer Form (#2148)

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -14,6 +14,10 @@ class FormsController < ApplicationController
     render 'feedback'
   end
 
+  def volunteer
+    render 'volunteer'
+  end
+
   def validate_recaptcha
     uri = URI.parse('https://www.google.com/recaptcha/api/siteverify')
 
