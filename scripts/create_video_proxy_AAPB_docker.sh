bced9bd014f6d74a8bd5704206b4e63b91027001
mivillesvik <38049679+mivillesvik@users.noreply.github.com>
2021-04-27 15:13:11 -0400

    Create create_video_proxy_AAPB_docker.sh
    script for creating video proxies in a dockerized ffmpeg environment

---
+++ scripts/create_video_proxy_AAPB_docker.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# run with two args:  first is input directory path and second is output directory path
+function usage()
+{
+	echo 'usage:  '$(basename $0)': /path/to/input/ /path/to/output/'
+	echo '--';
+	echo 'this script explores the input file system and selectively makes proxy versions of its media files in the output file system';
+	echo 'NOTE: both arguments must be usable directories';
+	exit;
+}
+
+# sanity checks
+if [ "$#" -ne 2 ];
+then
+	echo "ERROR:  two arguments are required";
+	usage;
+fi;
+
+if [ ! -d "$1" -o ! -r "$1" ];
+then
+	echo "ERROR:  $1 is not a readable directory";
+	usage;
+fi;
+
+if [ ! -d "$2" -o ! -w "$2" ];
+then
+	echo "ERROR:  $2 is not a writable directory";
+	usage;
+fi;
+
+inDir="$1";
+outDir="$2";
+
+cd "$inDir";
+
+OLDIFS=$IFS;
+IFS=`echo -en '\n\b'`;
+
+for i in `find . -type f | grep -i '\(mp4\|mov\|mts\|vob\|mpg\|webm\|mkv\|mxf\|m4v\|raw\|avi\)$'` ;
+do 
+	newDir=`dirname "$i"`; # modify this if you want to include the basename of the input argument directory
+	mkdir -p "$inDir/proxies";
+	if mediainfo "$i" | grep '16:9'; then
+		docker run -v $(pwd):$(pwd) -w $(pwd) jrottenberg/ffmpeg -i "$i" -vcodec libx264 -pix_fmt yuv420p -b:v 711k -s 640:360 -acodec aac -ac 2 -b:a 128k -metadata creation_time=now "proxies/$(basename $i)".mp4;
+	else 
+		docker run -v $(pwd):$(pwd) -w $(pwd) jrottenberg/ffmpeg -i "$i" -vcodec libx264 -pix_fmt yuv420p -b:v 711k -s 480:360 -acodec aac -ac 2 -b:a 128k -metadata creation_time=now "proxies/$(basename $i)".mp4;
+	fi
+done ;
+cd
+find "$inDir/proxies" -type f -exec mv {} "$outDir" \;
+rm -r "$inDir/proxies"
+IFS=$OLDIFS
