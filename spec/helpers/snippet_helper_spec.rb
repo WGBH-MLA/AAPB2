7007baad60f8ef2ed401dfcfacbf733c2a4e4daa
Jason Corum <jason.corum@gmail.com>
2021-10-06 10:33:44 -0700

    Adds GitHub Actions for CI (#2332)
    * Creates GitHub Actions YML for Rspec Testing
    
    * Fixes Rubocop errors
    
    * Updates name of CI task

--- spec/helpers/snippet_helper_spec.rb
+++ spec/helpers/snippet_helper_spec.rb
@@ -19,27 +19,23 @@ describe SnippetHelper do
   let(:txt_example) { File.read('./spec/fixtures/transcripts/cpb-aacip-507-0000000j8w-transcript.txt') }
   let(:txt_transcript) { TranscriptFile.new(txt_url) }
 
-
   # queries are split up into no-stopword arrays in appl helper
-  let(:transcript_query_1) { [ ["ARKANSAS"] ] }
-  let(:transcript_query_2) { [ ["SENATOR", "PRYOR"] ] }
-  let(:transcript_query_3) { [ ["FILED", "FOR", "A", "DELAY"], ["NEVER", "GONNA", "GET", "MATCHED"] ] }
-  let(:transcript_query_4) { [ ["TWO", "DOZEN", "FINE", "POTENTIAL", "NOMINEES", "FOR", "THE", "POSITION", "OF", "SECRETARY", "OF", "THE", "INTERIOR"] ] }
-
+  let(:transcript_query_1) { [["ARKANSAS"]] }
+  let(:transcript_query_2) { [%w(SENATOR PRYOR)] }
+  let(:transcript_query_3) { [%w(FILED FOR A DELAY), %w(NEVER GONNA GET MATCHED)] }
+  let(:transcript_query_4) { [%w(TWO DOZEN FINE POTENTIAL NOMINEES FOR THE POSITION OF SECRETARY OF THE INTERIOR)] }
 
   let(:srt_example) { File.read('./spec/fixtures/captions/srt/srt_example.srt') }
   let(:caption_file) { CaptionFile.new("1a2b") }
 
-
   # single
-  let(:transcript_snippet_1) { TimecodeSnippet.new('cpb-aacip-111-21ghx7d6', transcript_query_1, json_transcript.plaintext, JSON.parse(json_transcript.content)["parts"] ) }
+  let(:transcript_snippet_1) { TimecodeSnippet.new('cpb-aacip-111-21ghx7d6', transcript_query_1, json_transcript.plaintext, JSON.parse(json_transcript.content)["parts"]) }
   # compound
-  let(:transcript_snippet_2) { TimecodeSnippet.new('cpb-aacip-111-21ghx7d6', transcript_query_2, json_transcript.plaintext, JSON.parse(json_transcript.content)["parts"] ) }
+  let(:transcript_snippet_2) { TimecodeSnippet.new('cpb-aacip-111-21ghx7d6', transcript_query_2, json_transcript.plaintext, JSON.parse(json_transcript.content)["parts"]) }
   # caption
-  let(:transcript_snippet_3) { Snippet.new('cpb-aacip-111-21ghx7d6', transcript_query_3, caption_file.text ) }
+  let(:transcript_snippet_3) { Snippet.new('cpb-aacip-111-21ghx7d6', transcript_query_3, caption_file.text) }
   # txt
-  let(:transcript_snippet_4) { Snippet.new('cpb-aacip-507-0000000j8w', transcript_query_4, txt_transcript.plaintext ) }
-
+  let(:transcript_snippet_4) { Snippet.new('cpb-aacip-507-0000000j8w', transcript_query_4, txt_transcript.plaintext) }
 
   before do
     # Stub requests so we don't actually have to fetch them remotely. But note
@@ -48,24 +44,20 @@ describe SnippetHelper do
     WebMock.stub_request(:get, json_url).to_return(body: json_example)
     WebMock.stub_request(:get, txt_url).to_return(body: txt_example)
 
-
     CaptionFile.any_instance.stub(:captions_src).and_return('https://s3.amazonaws.com/americanarchive.org/captions/1a2b.srt')
     WebMock.stub_request(:get, caption_file.srt_url).to_return(body: srt_example)
   end
 
   describe '#new' do
     it 'initializes with the expected attrs' do
-
       expect(transcript_snippet_1.snippet).to eq(" FOR THIS 15TH ANNIVERSARY CELEBRATION AND DEDICATION CEREMONY IS MR GEORGE CAMPBELL CHAIRMAN OF THE <mark>ARKANSAS</mark> EDUCATIONAL TELEVISION COMMISSION GOOD AFTERNOON DISTINGUISHED GUESTS LADIES AND GENTLEMEN ")
       expect(transcript_snippet_1.match_timecode).to eq("50.24")
     end
 
     it 'initializes with expected attrs for compound query' do
-
       expect(transcript_snippet_2.snippet).to eq(" NOW I MAKE NO APOLOGIES FOR STORIES I MAY OR MAY NOT TALE CAN CERTAINLY RAISE BUT I JUST CANT THINK <mark>SENATOR PRYOR</mark> ALL TALKING ABOUT LEE REEVES FROM A PARTICULARLY GOVERNMENTAL AND STATE GOVERNMENT")
       expect(transcript_snippet_2.match_timecode).to eq("2061.79")
     end
-
 
     it 'initializes with expected attrs for caption file' do
       expect(transcript_snippet_3.snippet).to eq(" THE SUMMER OF 1958 THAT ALLOWED THE LOST YEAR TO HAPPEN THE FIRST ONE WAS THAT THE SCHOOL BOARD HAD <mark>FILED FOR A DELAY</mark>  A NUMBER OF BUSINESS LEADERS PERSUADED A MAJORITY OF THE MEMBERS OF THE SCHOOL")
@@ -73,7 +65,7 @@ describe SnippetHelper do
 
     it 'initializes with expected attrs for txt file' do
       expect(transcript_snippet_4.snippet).to eq("AT THE OLD EXECUTIVE OFFICE BUILDING PRES RONALD REAGAN AFTER EXAMINING THE RECORDS OF MORE THAN <mark>TWO DOZEN FINE POTENTIAL NOMINEES FOR THE POSITION OF SECRETARY OF THE INTERIOR</mark> I HAVE DECIDED TO")
-    end    
+    end
   end
 
   describe '#url_at_timecode' do
@@ -88,7 +80,7 @@ describe SnippetHelper do
     # end
 
     it 'uses stopwords.txt to remove words not used in actual search' do
-      expect(query_to_terms_array(%(extremist is cheddar "president of the Eisenhower"))).to eq([["PRESIDENT", "OF", "THE", "EISENHOWER"], ["EXTREMIST"], ["CHEDDAR"]])
+      expect(query_to_terms_array(%(extremist is cheddar "president of the Eisenhower"))).to eq([%w(PRESIDENT OF THE EISENHOWER), ["EXTREMIST"], ["CHEDDAR"]])
     end
   end
   after(:all) do
