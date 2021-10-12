ef4abd26ab1562b05c33c594fd451246c7083697
Jason Corum <jason.corum@gmail.com>
2021-10-12 10:24:44 -0700

    Creates AAPB.valid_id?(id), Refactors delete_records.rb to use a class and adds tests (#2326)
    * Refactors delete_records.rb to use a class and adds tests
    
    * Adds usage comment on delete_records.rb script

---
+++ scripts/lib/deleter.rb
@@ -0,0 +1,33 @@
+require_relative '../../lib/rails_stub'
+require_relative '../../app/models/exhibit'
+require_relative '../lib/downloader'
+require_relative '../lib/cleaner'
+require_relative '../lib/pb_core_ingester'
+require 'logger'
+require 'rake'
+
+class Deleter
+
+  def initialize(ids)
+    @ids = validate_ids(ids)
+  end
+
+  def delete
+    ingester.delete_records(@ids)
+  end
+
+  private
+
+  def validate_ids(ids)
+    raise 'Invalid arguments for deleting AAPB records. Must be an Array of IDs.' unless ids.is_a? Array
+    unless ids.all? { |id| AAPB.valid_id?(id) }
+      puts "Got a non-GUID argument! That's not cool!"
+      exit 1
+    end
+    ids
+  end
+
+  def ingester
+    PBCoreIngester.new
+  end
+end
