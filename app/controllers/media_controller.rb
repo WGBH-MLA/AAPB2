00ccd6f7f9143422a3f07b20c49ec3b7503e0f29
foglabs <foglaboratories@gmail.com>
2021-10-29 12:31:03 -0400

    'fix rubocop complaints'

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -13,7 +13,7 @@ class MediaController < ApplicationController
 
     # if can?(:play, pbcore) && (current_user.aapb_referer? || current_user.embed?)
     if can?(:access_media_url, pbcore)
-      @client = SonyCiApi::Client.new( Rails.root + 'config/ci.yml' )
+      @client = SonyCiApi::Client.new(Rails.root + 'config/ci.yml')
       # OAuth credentials expire: otherwise it would make sense to cache this instance.
       ci_id = pbcore.ci_ids[(params['part'] || 1).to_i - 1]
       redirect_to @client.asset_stream_hls_url(ci_id)
