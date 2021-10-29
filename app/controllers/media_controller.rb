bb2e4e160daef49590bb29b71f7ba9496541d503
Henry Fog <foglaboratories@gmail.com>
2021-10-29 12:57:40 -0400

    2346 switch to streaming (#2354)
    Use HLS streaming rather than progressive download
    
    Streaming from Sony Ci makes it a bit harder to capture media from our site, so we are
    switching to that.
    
    To do this, we're switching to use the new Sony Ci API gem which provides a cleaner interface to the
    underlying Sony Ci API, including an method for fetching HLS streaming URLs, and will allow for more
    interaction with Sony Ci in the future.
    
    Also removes a one-off script and spec dependent on the old Sony Ci API gem, but we don't need it anymore.
    
    Closes #2346.

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -13,9 +13,10 @@ class MediaController < ApplicationController
 
     # if can?(:play, pbcore) && (current_user.aapb_referer? || current_user.embed?)
     if can?(:access_media_url, pbcore)
-      ci = SonyCiBasic.new(credentials_path: Rails.root + 'config/ci.yml')
+      @client = SonyCiApi::Client.new(Rails.root + 'config/ci.yml')
       # OAuth credentials expire: otherwise it would make sense to cache this instance.
-      redirect_to ci.download(pbcore.ci_ids[(params['part'] || 1).to_i - 1])
+      ci_id = pbcore.ci_ids[(params['part'] || 1).to_i - 1]
+      redirect_to @client.asset_stream_hls_url(ci_id)
     else
       render nothing: true, status: :unauthorized
     end
