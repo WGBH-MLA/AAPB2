060023832c7e5add70d98d77a3ef2bec9ae59571
Andrew Myers <andrew_myers@wgbh.org>
2022-04-07 09:06:41 -0400

    Fixes error when transcript file is missing (#2410)
    Changes ExternalFile
    * Rescues from all errors on head request, including Bad URI, logs the error msg,
      and returns false. Does not re-raise the error.
    * Uses immutable attr_readers rather than instance vars.
    * Always cache result of #head_file in #file_present? whether :force_check is true or not.
    
    Changes ExternalFile test
    * Uses a test Rails cache which turned out to be fairly straightforward.
    * Changes "describe" text to reflect the methods under test.
    * Add specs for testing the :force_check param of #file_present? method.
    * Creates 2 contexts for testing file_present? and file_content: one context where the
      file is at the URL, and one where it's not. This allows us to mock twice, instead of
      4 times.
    
    closes #2408

--- spec/lib/external_file_spec.rb
+++ spec/lib/external_file_spec.rb
@@ -1,45 +1,115 @@
-require 'external_file'
-require 'httparty'
+require 'rails_helper'
 
 describe ExternalFile do
-  before :all do
-    # WebMock is disabled by default, but we use it for these tests.
-    # Note that it is re-disabled in an :after hook below.
-    WebMock.enable!
-  end
-
-  json_url = 'https://s3.amazonaws.com/americanarchive.org/transcripts/cpb-aacip-111-21ghx7d6/cpb-aacip-111-21ghx7d6-transcript.json'
-  let(:json_example) { File.read('./spec/fixtures/transcripts/cpb-aacip-111-21ghx7d6-transcript.json') }
-  let(:external_file) { ExternalFile.new("transcript", "cpb-aacip-111-21ghx7d6", json_url) }
+  # Enable/disable Webmock before/after this test context.
+  before(:all) { WebMock.enable! }
+  after(:all) { WebMock.disable! }
 
   before do
-    # Stub requests so we don't actually have to fetch them remotely. But note
-    # that this requires that the files have been pulled down and saved in
-    # ./spec/fixtures/transcripts/ with the same filename they have in S3.
-    WebMock.stub_request(:any, json_url).to_return(status: 200)
-    WebMock.stub_request(:get, json_url).to_return(body: json_example)
-
-    # rather than stub the whole rails cache infrastructure, just bypass
-    ExternalFile.any_instance.stub(:file_present?).and_return(true)
+    test_cache = ActiveSupport::Cache.lookup_store(:memory_store)
+    allow(Rails).to receive(:cache).and_return(test_cache)
+    Rails.cache.clear
   end
 
-  describe '#new' do
-    it 'heads the external file' do
-      # kind of dumb, stubbed to 200, but tests the struture of class
-      expect(external_file.file_present?).to eq(true)
+  let(:guid) { "cpb-aacip-111-21ghx7d6" }
+  let(:url) { "https://s3.amazonaws.com/americanarchive.org/transcripts/#{guid}/#{guid}-transcript.json" }
+  let(:content) { File.read('./spec/fixtures/transcripts/cpb-aacip-111-21ghx7d6-transcript.json') }
+  let(:external_file) { ExternalFile.new("transcript", guid, url) }
+
+  describe '#guid' do
+    context 'when initialized using a GUID with the slash-style' do
+      let(:guid) { 'cpb-aacip/111-21ghx7d6' }
+      it 'returns normalized GUID' do
+        expect(external_file.guid).to eq 'cpb-aacip-111-21ghx7d6'
+      end
     end
 
-    it 'returns the external content' do
-      expect(external_file.file_content).to eq(json_example)
+    context 'when initialized using a GUID with underscore style' do
+      let(:guid) { 'cpb-aacip_111-21ghx7d6' }
+      it 'returns normalized GUID' do
+        expect(external_file.guid).to eq 'cpb-aacip-111-21ghx7d6'
+      end
     end
+  end
 
+  describe '#cache_key' do
     it 'builds a correct cache key for external file' do
       expect(external_file.cache_key).to eq("transcript/cpb-aacip-111-21ghx7d6")
     end
   end
 
-  after(:all) do
-    # Re-disable WebMock so other tests can use actual connections.
-    WebMock.disable!
+  context 'when an HTTP request to the URL returns a 200 status' do
+    before do
+      WebMock.stub_request(:head, url).to_return(status: 200)
+      WebMock.stub_request(:get, url).to_return(body: content, status: 200)
+    end
+
+    describe '#file_present?' do
+      it 'returns true' do
+        expect(external_file.file_present?).to eq true
+      end
+
+      context 'when called multile times' do
+        let(:num_calls) { 3 }
+        before do
+          allow(external_file).to receive(:head_file).and_return(true)
+          num_calls.times { external_file.file_present?(force_check: force_check) }
+        end
+
+        context 'when :force_check is false' do
+          let(:force_check) { false }
+          it 'only calls #head_file once' do
+            expect(external_file).to have_received(:head_file).exactly(1).times
+          end
+        end
+
+        context 'when :force_check is true' do
+          let(:force_check) { true }
+          it 'calls #head_file every time' do
+            expect(external_file).to have_received(:head_file).exactly(num_calls).times
+          end
+        end
+      end
+    end
+
+    describe '#file_content' do
+      it 'returns the external content' do
+        expect(external_file.file_content).to eq(content)
+      end
+    end
+  end
+
+  context "when an HTTP request to the URL returns a non-200 status" do
+    before do
+      # TODO: Figure out an elegant way to test all non-200 responses.
+      WebMock.stub_request(:head, url).to_return(status: 400)
+    end
+
+    describe 'file_present?' do
+      it 'returns false' do
+        expect(external_file.file_present?).to eq false
+      end
+    end
+
+    describe 'file_content' do
+      it 'returns nil' do
+        expect(external_file.file_content).to be_nil
+      end
+    end
+  end
+
+  context 'when the url is bad' do
+    let(:url) { 'not a url' }
+    describe 'file_present?' do
+      it 'returns false' do
+        expect(external_file.file_present?).to eq false
+      end
+    end
+
+    describe 'file_content' do
+      it 'returns nil' do
+        expect(external_file.file_content).to eq nil
+      end
+    end
   end
 end
