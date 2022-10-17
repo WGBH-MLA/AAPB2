f8441b1ba29ec978b9b8ab54199d04040ff22083
foglabs <foglaboratories@gmail.com>
2022-10-17 15:11:12 -0400

    'controller'

--- app/controllers/educator_resources_controller.rb
+++ app/controllers/educator_resources_controller.rb
@@ -1,2 +1,15 @@
-class EducatorResourcesController < ApplicationController
+require_relative '../models/exhibit'
+
+class EducatorResourcesController < OverrideController
+  def index
+    @all_resource_sets = EducatorResource.all_resource_sets
+    @page_title = 'Educator Resources'
+  end
+
+  def show
+    @educator_resource = EducatorResource.find_by_path(params[:path])
+    raise ActionController::RoutingError.new('Not Found') unless @educator_resource
+    @page_title = @educator_resource.title
+    params[:path] = nil
+  end
 end
