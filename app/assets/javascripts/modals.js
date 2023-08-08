5fdd240995ab22e43b484092c4fdb8f38bf78eab
Henry Fog <foglaboratories@gmail.com>
2023-08-08 10:44:15 -0400

    Consolidate modal js (#2623)
    * Update le-show.md
    
    updating linked records
    
    * 'consolidate js for modals and give ORR modal precedence'
    
    * 'remove cruft'
    
    ---------
    
    Co-authored-by: mivillesvik <38049679+mivillesvik@users.noreply.github.com>

---
+++ app/assets/javascripts/modals.js
@@ -0,0 +1,36 @@
+$(function() {
+  // whats the path
+  let path = new URL(window.location.href).pathname
+
+  // did we already trigger these modals
+  let orr = getCookie("orr_rules_of_use")
+  let nws = getCookie("newsletter")
+  // duh
+  let triggered_orr = false
+
+  if(path.includes("/catalog/") && orr != "y"){
+    $('#rules-modal').modal({keyboard: true});
+    triggered_orr = true
+  }
+  if(!triggered_orr && nws != "y"){
+    $('#newsletter-modal').modal({keyboard: true});
+    // do cookie here because this one should not require affirmation to stop showing
+    document.cookie = 'newsletter=y;max-age=15770000'
+  }
+});
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
