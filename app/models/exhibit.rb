8db93c0441a2b4aba0eedf92a90f30dccfe78701
Henry Fog <foglaboratories@gmail.com>
2023-10-10 13:44:58 -0400

    'wrong case for overview title in exhibit section menu' (#2672)

--- app/models/exhibit.rb
+++ app/models/exhibit.rb
@@ -80,16 +80,20 @@ class Exhibit < Cmless
     end
   end
 
+  def overview_title
+    if path == "empoderamiento-latino"
+      "Resumen"
+    else
+      "Overview"
+    end
+  end
+
   def display_title
     # this is just to wrap the behavior around parent/child sections and es language exhibit's special overview titlee
     if subsection?
       title
     else
-      if path == "empoderamiento-latino"
-        "Resumen"
-      else
-        "Overview"
-      end
+      overview_title
     end
   end
 
