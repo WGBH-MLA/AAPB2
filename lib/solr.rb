40628e7f92dd230f31461db03c7d5b1a55da4501
Harpo Harbert <ryan_harbert@wgbh.org>
2024-04-19 13:09:05 -0700

    Try: blacklight-core url in initializer

--- lib/solr.rb
+++ lib/solr.rb
@@ -11,7 +11,7 @@ class Solr
     environment = ENV['RAILS_ENV'] || DEFAULT
     conf = YAML.load_file(Rails.root + 'config/blacklight.yml')
     solr_url = ENV['SOLR_URL'] || conf[environment]['url']
-    @connect = RSolr.connect(url: solr_url)
+    @connect = RSolr.connect(url: solr_url + 'blacklight-core')
   end
 
   attr_reader :connect
