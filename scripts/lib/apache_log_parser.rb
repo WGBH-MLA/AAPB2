b458183231c0dd72316f52e6d0687cf0050568f9
Andrew Myers <afredmyers@gmail.com>
2025-02-12 15:52:47 -0500

    Adds params to apache log parser script:
    * number of octets from IP to group log entries by
    * number of results to print
    
    These extra params help us cast more, smaller nets to block (hopefully) more bots, fewer humans.

--- scripts/lib/apache_log_parser.rb
+++ scripts/lib/apache_log_parser.rb
@@ -119,34 +119,56 @@ class BadBotConfigLines
   def to_s
     indent + ips.map do |ip|
       ip += '.' if ip.count('.') < 3
-      ip.gsub!('.', '\.')
+      ip = ip.gsub('.', '\.')
       "SetEnvIfNoCase Remote_Addr \"^#{ip}\" bad_bot"
     end.join("\n#{indent}")
   end
 end
 
-if $PROGRAM_NAME == __FILE__
-  glob = ARGV[0].to_s
-  raise "Pass in a glob of Apache log files to parse" if glob.empty?
-  files = Dir.glob(glob)
-  raise "No files found in '#{ARGV[0]}'" if files.empty?
 
-  p = ApacheLogParser.new(files: files)
+begin
+  if $PROGRAM_NAME == __FILE__
+    puts "Parsing Apache Log on " + DateTime.now.new_offset("-05:00").strftime('%Y-%m-%d %I:%M:%S %P')
 
-  bad_bots = p.entries.select do |e|
-    e.ip != '::1'
-  end.select do |e|
-    e.request_uri =~ /catalog\?/
-  end.group_by do |e|
-    e.ip.split('.').first(2).join('.')
-  end.map do |ip, entries|
-    [ip, entries.count]
-  end.sort_by do |_ip, count|
-    count
-  end.reverse.first(25).to_h
+    # Grab parameters form command line args
+    glob = ARGV[0].to_s
+    octets = (ARGV[1] || 2).to_i
+    count = (ARGV[2] || 25).to_i
 
-  # Output the lines for bad bot configuration
-  puts "Worst #{bad_bots.count} Offenders:\n#{bad_bots.map { |k, v| "#{k}: #{v}" }.join("\n")}\n\n"
-  puts "Apache config to flag them:\n\n"
-  puts BadBotConfigLines.new(ips: bad_bots.keys)
+
+    # Get thre files, raise if none found
+    raise "Pass in a glob of Apache log files to parse" if glob.empty?
+    files = Dir.glob(glob)
+    raise "No files found in '#{ARGV[0]}'" if files.empty?
+
+    raise "Octests (2nd argument) must be a number between 1 and 4" unless (1..4).include?(octets)
+    raise "Count (3rd argument) must be a number greater than 0" unless count > 0
+
+    p = ApacheLogParser.new(files: files)
+
+    bad_bots = p.entries.select do |e|
+      e.ip != '::1'
+    end.select do |e|
+      e.request_uri =~ /catalog\?/
+    end.group_by do |e|
+      e.ip.split('.').first(octets).join('.')
+    end.map do |ip, entries|
+      [ip, entries.count]
+    end.sort_by do |_ip, count|
+      count
+    end.reverse.first(count).to_h
+
+    # Output the lines for bad bot configuration
+    puts "Worst #{bad_bots.count} Offenders:\n#{bad_bots.map { |k, v| "#{k}: #{v}" }.join("\n")}\n\n"
+    puts "Apache config to flag them:\n\n"
+    puts BadBotConfigLines.new(ips: bad_bots.keys)
+  end
+rescue => e
+  if ['true', '1', 'yes', 'y'].include?(ENV['DEBUG'].to_s.downcase)
+    puts "#{e.class}: #{e.message}"
+    puts e.backtrace.join("\n")
+  else
+    puts e.message
+    exit 1
+  end
 end
