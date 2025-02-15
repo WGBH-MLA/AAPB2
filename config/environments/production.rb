65178d751a204ea899cfff429745ce3897205659
Andrew Myers <afredmyers@gmail.com>
2025-02-15 00:49:12 -0500

    Set log level to debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :warn
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
