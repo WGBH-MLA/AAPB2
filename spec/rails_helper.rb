ab88c76590810483f7940ef5275dcdff02ebde2f
Erica Titkemeyer <erica_titkemeyer@wgbh.org>
2025-03-17 11:28:19 -0400

    update to account for turnstile

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,11 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  # Stub Turnstile verification to always return true for tests
+  config.before(:each) do
+    allow_any_instance_of(Turnstile::Client).to receive(:verify).and_return(true)
+  end
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
