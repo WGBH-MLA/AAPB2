3b4864b1c2b07c8b74401240a351d4a503aa3e59
Erica Titkemeyer <erica_titkemeyer@wgbh.org>
2025-03-19 01:48:59 +0000

    formatting

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,18 +21,17 @@ require_relative 'support/remote_ip_monkey_patch'
 #
 # Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
-# Checks for pending migrations before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.maintain_test_schema!
-
 RSpec.configure do |config|
   config.include TurnstileHelper, type: :request
-  config.before(:each, type :request) do
+  config.before(:each, type: :request) do
     bypass_turnstile
   end
+
+  # Checks for pending migrations before tests are run.
+  # If you are not using ActiveRecord, you can remove this line.
+  ActiveRecord::Migration.maintain_test_schema!
 end
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
--- spec/support/turnstile_helper.rb
+++ spec/support/turnstile_helper.rb
@@ -1,6 +1,7 @@
+# This module provides helper methods for bypassing the turnstile verification in tests.
 module TurnstileHelper
-    def bypass_turnstile
-      cookies.encrypted[:turnstile_verified] = true
-    end
+  def bypass_turnstile
+    cookies.encrypted[:turnstile_verified] = true
   end
+end
   
