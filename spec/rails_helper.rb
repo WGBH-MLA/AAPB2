8ae891232e2723c52a9f8f5505257094932faace
Andrew Myers <afredmyers@gmail.com>
2025-03-20 12:41:11 -0400

    Try setting turnstile cookie in before(:suite) rspec hook

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -49,7 +49,7 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
-  config.before(:each, type: :request) do
+  config.before(:suite) do
     cookies.encrypted[:turnstile_verified] = true
   end
 end
