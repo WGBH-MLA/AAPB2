71aa892cfa28c0de98405001c2429870d2557721
Andrew Myers <afredmyers@gmail.com>
2025-03-20 12:31:47 -0400

    Set turnstile cookie to true for all requests in specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -48,4 +48,8 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  config.before(:each, type: :request) do
+    cookies.encrypted[:turnstile_verified] = true
+  end
 end
