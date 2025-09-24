979cb9b44b536d1b032e77f47b4b95194c7251f1
Erica Titkemeyer <erica_titkemeyer@wgbh.org>
2025-09-24 12:33:38 -0400

    adds back missing chunk

--- spec/features/catalog_spec.rb
+++ spec/features/catalog_spec.rb
@@ -95,13 +95,12 @@ describe 'Catalog' do
       visit "/catalog?f[access_types][]=#{PBCorePresenter::ALL_ACCESS}&q=\"cpb-aacip-1234\""
       expect(page.status_code).to eq(200)
       expect_count(1, page.text)
-
-      %w(
-        Nova\; Gratuitous\ Explosions\; 3-2-1\; Kaboom!
-        Date:\ 2000-01-01
-        Producing\ Organization:\ WGBH
-        Best\ episode\ ever!
-      ).each do |field|
+      [
+        'Nova; Gratuitous Explosions; 3-2-1; Kaboom!',
+        'Date: 2000-01-01',
+        'Producing Organization: WGBH',
+        'Best episode ever!'
+      ].each do |field|
         expect(page).to have_text(field), missing_page_text_custom_error(field, page.current_path)
       end
 
@@ -479,6 +478,81 @@ describe 'Catalog' do
       visit 'catalog/cpb-aacip_169-9351chfc'
       expect_all_the_text('clean-audio-digitized.xml')
       expect_audio(poster: '/thumbs/AUDIO.png')
+    end
+        it 'apologizes if no access' do
+      visit '/catalog/cpb-aacip-80-12893j6c'
+      # No need to click through
+      expect_all_the_text('clean-bad-essence-track.xml')
+      expect(page).to have_text('This content is not available.'), missing_page_text_custom_error('This content is not available.', page.current_path)
+      expect_no_media
+    end
+
+    it 'links to exhibit' do
+      visit '/catalog/cpb-aacip_111-21ghx7d6'
+      expect(page).to have_text('This record is featured in'), missing_page_text_custom_error('This record is featured in', page.current_path)
+      expect_video(poster: s3_thumb('cpb-aacip-111-21ghx7d6'))
+    end
+
+    it 'has a transcript if expected' do
+      visit '/catalog/cpb-aacip_111-21ghx7d6'
+      expect_transcript
+    end
+
+    it 'has no transcript if expected' do
+      visit '/catalog/cpb-aacip_508-g44hm5390k'
+      expect_no_transcript
+    end
+
+    describe 'access control' do
+      it 'has warning for non-us access' do
+        ENV['RAILS_TEST_IP_ADDRESS'] = '0.0.0.0'
+        visit 'catalog/cpb-aacip_37-16c2fsnr'
+        ENV.delete('RAILS_TEST_IP_ADDRESS')
+        expect_all_the_text('clean-every-title-is-episode-number.xml')
+        expect(page).to have_text('not available at your location.'), missing_page_text_custom_error('not available at your location.', page.current_path)
+        expect_no_media
+      end
+
+      it 'has warning for off-site access' do
+        ENV['RAILS_TEST_IP_ADDRESS'] = Resolv.getaddress('umass.edu')
+        visit 'catalog/cpb-aacip_111-21ghx7d6'
+        ENV.delete('RAILS_TEST_IP_ADDRESS')
+        expect_all_the_text('clean-exhibit.xml')
+        expect(page).to have_text('only available at GBH and the Library of Congress'), missing_page_text_custom_error('only available at GBH and the Library of Congress. ', page.current_path)
+        expect_no_media
+      end
+
+      it 'requires click-thru for ORR items' do
+        ENV['RAILS_TEST_IP_ADDRESS'] = Resolv.getaddress('umass.edu')
+        visit 'catalog/cpb-aacip_37-16c2fsnr'
+        ENV.delete('RAILS_TEST_IP_ADDRESS')
+        expect(page).to have_text('Online Reading Room Rules of Use'), missing_page_text_custom_error('Online Reading Room Rules of Use', page.current_path)
+      end
+
+      it 'has both playlist navigation options when applicable' do
+        visit 'catalog/cpb-aacip_512-0r9m32nw1x'
+        expect(page).to have_css('div#playlist')
+        expect(page).to have_text('Part 1'), missing_page_text_custom_error('Part 1', page.current_path)
+      end
+
+      it 'has next playlist navigation option when first item in playlist' do
+        visit 'catalog/cpb-aacip_512-gx44q7rk20'
+        expect(page).to have_css('div#playlist')
+        expect(page).not_to have_text('Part 0')
+        expect(page).to have_text('Part 2')
+      end
+
+      it 'has previous playlist navigation option when last item in playlist' do
+        visit 'catalog/cpb-aacip_512-w66930pv96'
+        expect(page).to have_css('div#playlist')
+        expect(page).to have_text('Part 2'), missing_page_text_custom_error('Part 2', page.current_path)
+        expect(page).not_to have_text('Part 4'), found_page_text_custom_error('Part 4', page.current_path)
+      end
+
+      it 'should not have #playlist when not in playlist' do
+        visit 'catalog/cpb-aacip_111-21ghx7d6'
+        expect(page).not_to have_css('div#playlist')
+      end
     end
   end
 
