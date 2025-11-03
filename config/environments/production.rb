ce43a4078b828f9881c37c95e46bb8a05c1079b8
Erica Titkemeyer <erica_titkemeyer@wgbh.org>
2025-11-03 11:08:37 -0500

    rubocop fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,6 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-
   # Generate digests for assets URLs.
   config.assets.digest = true
 
