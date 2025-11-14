ff9f2e9d9f05c10659607b7c92711fc335bcf2e6
Erica Titkemeyer <erica_titkemeyer@wgbh.org>
2025-11-14 14:51:33 -0500

    updates skip_tos to include non-us users accessing global media

--- spec/models/abilities/pbcore/cannot_skip_tos_spec.rb
+++ spec/models/abilities/pbcore/cannot_skip_tos_spec.rb
@@ -7,17 +7,13 @@ describe Ability do
 
   context 'for PBCore records with transcripts' do
     let(:public_pbcore_record) { PBCorePresenter.new(File.read('./spec/fixtures/pbcore/access-level-public.xml')) }
-    let(:protected_pbcore_record) { PBCorePresenter.new(File.read('./spec/fixtures/pbcore/access-level-protected.xml')) }
+   #not clear why/if we need to test for this? let(:protected_pbcore_record) { PBCorePresenter.new(File.read('./spec/fixtures/pbcore/access-level-protected.xml')) }
 
     context 'when User is offsite; User has affirmed TOS; User is in US; user is a bot' do
       let(:user) { instance_double(User, 'onsite?' => false, 'affirmed_tos?' => true, 'usa?' => true, 'bot?' => true) }
 
       it 'skip_tos returns true for a public record' do
         expect(ability).to be_able_to(:skip_tos, public_pbcore_record)
-      end
-
-      it 'skip_tos returns false for a protected record' do
-        expect(ability).to be_able_to(:skip_tos, protected_pbcore_record)
       end
     end
 
@@ -27,10 +23,6 @@ describe Ability do
       it 'skip_tos returns true for a public record' do
         expect(ability).to be_able_to(:skip_tos, public_pbcore_record)
       end
-
-      it 'skip_tos returns false for a protected record' do
-        expect(ability).to be_able_to(:skip_tos, protected_pbcore_record)
-      end
     end
 
     context 'when User is offsite; User has affirmed TOS; User is not in US; user is not a bot' do
@@ -39,26 +31,18 @@ describe Ability do
       it 'skip_tos returns true for a public record' do
         expect(ability).to be_able_to(:skip_tos, public_pbcore_record)
       end
-
-      it 'skip_tos returns true for a protected record' do
-        expect(ability).to be_able_to(:skip_tos, protected_pbcore_record)
-      end
     end
 
     context 'when User is offsite; User has not affirmed TOS; User is not in US; user is not a bot' do
       let(:user) { instance_double(User, 'onsite?' => false, 'affirmed_tos?' => false, 'usa?' => false, 'bot?' => false) }
 
-      it 'skip_tos returns true for a public record' do
-        expect(ability).to be_able_to(:skip_tos, public_pbcore_record)
-      end
-
-      it 'skip_tos returns true for a protected record' do
-        expect(ability).to be_able_to(:skip_tos, protected_pbcore_record)
+      it 'skip_tos returns false for a public record' do
+        expect(ability).not_to be_able_to(:skip_tos, public_pbcore_record)
       end
     end
 
     context 'when User is onsite' do
-      let(:user) { instance_double(User, 'onsite?' => true) }
+      let(:user) { instance_double(User, 'onsite?' => true, 'affirmed_tos?' => false, 'usa?' => true, 'bot?' => false) }
 
       it 'skip_tos returns true for a public record' do
         expect(ability).to be_able_to(:skip_tos, public_pbcore_record)
