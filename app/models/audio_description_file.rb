7d753b2bd8728a380a4aefe0cad88f1a5d36145a
Erica Titkemeyer <erica_titkemeyer@wgbh.org>
2025-12-10 14:31:36 -0500

    missing end

--- app/models/audio_description_file.rb
+++ app/models/audio_description_file.rb
@@ -9,7 +9,8 @@ class AudioDescriptionFile < ExternalFile
     super("audio_description_hls", @id, @hls_src)
   end
 
-def file_present?
-  # optionally check S3 if master.m3u8 exists
-  true
+  def file_present?
+    # optionally check S3 if master.m3u8 exists
+    true
+  end
 end
