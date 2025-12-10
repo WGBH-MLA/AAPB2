568553e254b73ce2d6e8cbca30c5eb2bd89002c7
Erica Titkemeyer <erica_titkemeyer@wgbh.org>
2025-12-10 14:42:46 -0500

    removing references to AD options for mp3s

--- app/models/audio_description_file.rb
+++ app/models/audio_description_file.rb
@@ -1,16 +1,15 @@
 class AudioDescriptionFile < ExternalFile
   URL_BASE = 'https://s3.amazonaws.com/americanarchive.org/audio_descriptions'.freeze
 
-  attr_reader :id, :audio_src
+  attr_reader :id, :url
 
   def initialize(id)
     @id = normalize_guid(id)
-    @hls_src = "#{URL_BASE}/#{id}/master.m3u8"
-    super("audio_description_hls", @id, @hls_src)
+    @url = "#{URL_BASE}/#{id}/master.m3u8"
+    super("audio_description_hls", @id, @url)
   end
 
   def file_present?
-    # optionally check S3 if master.m3u8 exists
     true
   end
 end
