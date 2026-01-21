0a190093d9ac8aeb1409c22fa1bfff6a6dcdea60
Erica Titkemeyer <erica_titkemeyer@wgbh.org>
2026-01-21 15:45:52 -0500

    Disable rubocop metrics for perceived complexity
    Added a rubocop directive to disable perceived complexity metrics.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,13 +1,14 @@
 class Ability
   include CanCan::Ability
 
+  # rubocop:disable Metrics/PerceivedComplexity
+
   def initialize(user)
     can :skip_tos, PBCorePresenter do |pbcore|
       can?(:play, pbcore) &&
       !user.bot? &&
       (user.affirmed_tos? || user.authorized_referer?)
     end
-
 
     can :play, PBCorePresenter do |pbcore|
       (user.onsite? && (pbcore.public? || pbcore.protected?) && user.affirmed_tos?) ||
@@ -32,7 +33,7 @@ class Ability
       (user.onsite? && (pbcore.public? || pbcore.protected?)) ||
         ((user.embed? || user.aapb_referer? || user.authorized_referer?) && pbcore.public?)
     end
-      
+
     can [:api_access_transcript, :access_transcript], PBCorePresenter do |pbcore|
       !pbcore.private? && !pbcore.transcript_status.nil?
     end
